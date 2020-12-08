0c6ba134a78b90c11b37874b5d49db6c59be3e86
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2020-12-08 17:50:17 +0000

    LeetCode Sync Runtime - 1 ms (97.77%), Memory - 40.8 MB (100.00%)

--- 0605-can-place-flowers/solution.java
+++ 0605-can-place-flowers/solution.java
@@ -9,34 +9,24 @@ class Solution {
             else
                 return false;
         }
-            /*
-            return true; 
-        else if (len<=1 && n>1 && flowerbed[0]==1)
-            return false;
-        else if (len<=1 && n<=1 && flowerbed[0]==0  )
-            return true;
-        
-        */else        
+                  
         for(int i = 0 ; i<len ; i++){
-            //System.out.println(i);
+            
             if(flowerbed[0]==0 && flowerbed[1]==0){
                 flowerbed[0]=1;
-           // System.out.println("in first one loop");
+          
                 n--;
             }
             else if (flowerbed[i]==0 && i+2<len && flowerbed[i+1]==0   && flowerbed[i+2]==0){
                 flowerbed[i+1]=1;
                 n--;
-             //   for (int j: flowerbed)
-               //     System.out.println(j);
-                
-                //System.out.println("in middle one loop");
+            
             }
             
             else if(flowerbed[len-2]==0 && flowerbed[len-1]==0){
                 flowerbed[len-1]=1;
                 n--;
-            //    System.out.println("in end one loop");
+            
             }
             else 
                 continue;
