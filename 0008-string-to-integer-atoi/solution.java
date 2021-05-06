469ef32659e06174b950c43efa1371c02dea7bf1
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-05-06 20:38:44 +0000

    LeetCode Sync Runtime - 5 ms (8.27%), Memory - 39.2 MB (100.00%)

--- 0008-string-to-integer-atoi/solution.java
+++ 0008-string-to-integer-atoi/solution.java
@@ -1,61 +1,28 @@
 import java.math.BigInteger ;
 class Solution {
     public int myAtoi(String s) {
-        if(s.length()==0)
-            return 0;
         int i =0 ;
-        while(i<s.length() && s.charAt(i)==' ')
-            i++;
-        //System.out.println(i);
         boolean neg = false;
-        String num = "" ; 
+        long num = 0 ;
         
-        if (i<s.length() && s.charAt(i)=='-'){
-                neg = true;
-                i++;
-                //System.out.println(neg);
-            }
-            else if (i<s.length() && s.charAt(i)=='+'){
-                i++;
-            }
-            else{
+        if(s.length()==0) return 0;
+        
+        while(i<s.length() && s.charAt(i)==' ') i++;
                 
-            }
         
-        if (i<s.length() && ((s.charAt(i)>='0' && s.charAt(i)<='9') )  ){  
-            
-                while(i<s.length() &&  s.charAt(i)>='0'   &&                        s.charAt(i)<='9'){
-                    num= num+ s.charAt(i);
+        if (i<s.length() && (s.charAt(i)=='-'  || s.charAt(i)=='+'))
+                neg = s.charAt(i++)=='-' ? true : false;
+                         
+        //System.out.println(s.charAt(i));
+                while(i<s.length() &&  s.charAt(i)>='0'   &&  s.charAt(i)<='9'){
+                    num= num*10+ Integer.parseInt(""+s.charAt(i));
                     i++;
+                    if (num>Integer.MAX_VALUE)
+                        return neg ==true ? Integer.MIN_VALUE : Integer.MAX_VALUE;
                 }
                     
             
-            System.out.println(num);
-            int ans  = 0 ;
-            BigInteger bnum ;
-            if (neg==true)
-            bnum = new BigInteger("-"+num);
-            else
-                 bnum = new BigInteger(num);
-            
-            //long lnum =  Long.parseLong(num);
-            
-            BigInteger minvalue = new BigInteger(""+ Integer.MIN_VALUE);
-            BigInteger maxvalue = new BigInteger(""+ Integer.MAX_VALUE);
-            
-            if (neg ==true && bnum.compareTo(minvalue)<0)
-                return Integer.MIN_VALUE;
-            else if (neg ==false && bnum.compareTo(maxvalue)>0)
-                return Integer.MAX_VALUE;
-            else
-                ans = Integer.parseInt(""+bnum);
-                
-            //if (neg==true)
-             //   return ans*(-1);
-            //else
-                return ans ;
-        }
-        else 
-            return 0;
+            //System.out.println(num);
+            return neg==true ? (int)num*-1 :(int)num;
     }
 }
