3359cf7a9baec9d35bf9a3c33668d0e29ddf6107
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-05-06 19:09:38 +0000

    LeetCode Sync Runtime - 30 ms (44.69%), Memory - 39.4 MB (100.00%)

--- 0005-longest-palindromic-substring/solution.java
+++ 0005-longest-palindromic-substring/solution.java
@@ -1,33 +1,45 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length()<1)
-            return "";
-            String ans = "";
-            int start=0,end=0 ,m=0;
-            for(int i =0 ; i<s.length() ; i++){
-                int s1 = checkp(s,i,i);
-                int s2 = checkp(s,i,i+1);
-                
-                 m = Math.max(s1 ,s2);
-                System.out.println(i+" m is "+m);
-                if (m>end-start){
-                    start = i-(m-1)/2;
-                    end  = i+(m/2);
-                }
-                
-                
+        int len1 =0, len2 = 0 ; 
+        int temp = Integer.MIN_VALUE;
+        String ans = ""+s.charAt(0);
+        for (int i = 0; i<s.length()-1; i++){
+            String s1 = palin(i,i, s);
+            
+            String s2 ="";
+            if (s.charAt(i)==s.charAt(i+1))
+            s2 = palin(i,i+1,s);
+            
+           // System.out.println(s1+"   "+s2);
+            len1 =s1.length(); 
+            len2=s2.length();
+            
+            if (len1>temp){
+                ans = s1 ;
+                temp=len1;
             }
-        return s.substring(start, end +1);
-        
+            if (len2>temp){
+                ans = s2 ;
+                temp =len2; 
+            }
+                
+        }
+        return ans ;
     }
     
-    public static int checkp(String s ,int start , int end){
-        if (s==null || start>end ) return 0;
-        
-        while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end)){
-            start--;
-            end++;
+    
+    public static String palin(int i ,int j , String s){
+       
+        while(i-1>=0 && j+1<s.length() && s.charAt(i-1)==s.charAt(j+1) ){
+                    i-- ; j++ ;
+                    //System.out.println(i+"   "+j);
         }
-        return end -1 -start ;
+        return s.substring(i,j+1);
     }
+        
+        
+    
+    
+    
+    
 }
