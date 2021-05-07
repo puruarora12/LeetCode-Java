4162db397c99a2226ae63d7ab2495c18b0ef9f80
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-05-07 16:53:25 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 39.1 MB (100.00%)

--- 0617-merge-two-binary-trees/solution.java
+++ 0617-merge-two-binary-trees/solution.java
@@ -15,32 +15,17 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        //TreeNode temp1 = root1;
-        //TreeNode temp2 = root2;
-        TreeNode ans = new TreeNode();
-        if (root1 !=null) ans.val += root1.val;
-        if (root2 !=null) ans.val += root2.val;
-        if (root1==null && root2==null) return null;
-        else if (root1 !=null && root2!=null){
-            if(root1.left!=null || root2.left!=null ){
-            root1.left = root1.left!=null ? root1.left : new TreeNode(0);
-            root2.left = root2.left!=null ? root2.left : new TreeNode(0);
-            }else
-                ans.left=null;
-            
-            if(root1.right!=null || root2.right!=null ){
-            root1.right = root1.right!=null ? root1.right : new TreeNode(0);
-            root2.right = root2.right!=null ? root2.right : new TreeNode(0);
-            }else ans.right=null;
-            
-            if (root1.left!=null && root2.left!=null)
-            ans.left = mergeTrees(root1.left  , root2.left);
-            if (root1.right!=null && root2.right!=null)
-            ans.right = mergeTrees(root1.right , root2.right);
-            }
+        
+        if (root1 ==null && root2==null) return null;
+        else if (root1 == null ) return root2;
+        else if (root2 == null) return root1;
         else{
-            return root1==null ?  root2 : root1;
+            root1.val +=root2.val;
+            root1.left= mergeTrees(root1.left , root2.left);
+            root1.right= mergeTrees(root1.right , root2.right);
+            
         }
-        return ans ;
+        return root1;
+        
     }
 }
