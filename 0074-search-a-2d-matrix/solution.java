151c19821f3fe4de07a0a9ea703816e935014354
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-06-16 17:14:53 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 38.2 MB (100.00%)

--- 0074-search-a-2d-matrix/solution.java
+++ 0074-search-a-2d-matrix/solution.java
@@ -5,7 +5,7 @@ int cols  = matrix[0].length;
 
 int indexr = 0;
 int indexc = 0 ; 
-//System.out.println(rows+"   "+cols);
+
 int upperlimitr=rows-1;
 int upperlimitc = cols-1;
 
@@ -14,14 +14,12 @@ int midc = (int)Math.ceil((upperlimitc+indexc)/2.0);
         
  
 	while( (indexr<=upperlimitr) && (indexc<=upperlimitc) ){
-      //  System.out.println("inside");
-      //  System.out.println("indexr is "+indexr+" uperlimitr "+upperlimitr+"  midr "+midr);
-       // System.out.println("indexc is "+indexc+" uperlimitc "+upperlimitc+"  midc "+midc);
+     
 	if (matrix[midr][midc]==target){
         return true;
     }
 	else if (target>= matrix[midr][indexc]  &&  target<= matrix[midr][upperlimitc] ){
-		//System.out.println("It shoudlnot come in this less than cases ");
+
         if (target<matrix[midr][midc]){
             upperlimitc=midc-1;
             midc = (int)Math.ceil((upperlimitc+indexc)/2.0);
@@ -29,27 +27,18 @@ int midc = (int)Math.ceil((upperlimitc+indexc)/2.0);
 		else if (target>matrix[midr][midc]){ 
             indexc =midc+1;
 			 midc = (int)Math.ceil((upperlimitc+indexc)/2.0);
-			}
-
-			
+			}		
 	}
 	else if (target>matrix[midr][cols-1]){
 		indexr = midr+1;
-        //indexc = midc+1;
-      //  System.out.println("in this cases? ");
         midr = (int)Math.ceil((upperlimitr+indexr)/2.0);
-        //midc = (int)Math.ceil((upperlimitc+indexc)/2.0);
-        
-        
+    
     }
 	else if (target<matrix[midr][0]){
 		upperlimitr = midr-1;
-        //upperlimitc = midc-1;
         midr = (int)Math.ceil((upperlimitr+indexr)/2.0);
-       // System.out.println("It shoudl come in this less than cases ");
-       // midc = (int)Math.ceil((upperlimitc+indexc)/2.0);
     }
-        else return false;
+    else return false;
 	}
 
 return false;
