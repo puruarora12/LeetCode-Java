7dcd670689abe5db8c1a84a9bc19ce303114929b
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-06-16 18:51:04 +0000

    LeetCode Sync Runtime - 2 ms (87.85%), Memory - 52.5 MB (99.91%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       int ans[] = new int[nums.length];
-        Arrays.fill(ans ,1);
-        
-        int temp =nums[0] ;
-        
-        for(int i =1; i<nums.length ; i++){
-           ans[i] = temp;
-            temp *= nums[i];
-       } 
-        
-        temp=nums[nums.length-1];
-        
-        for( int j = nums.length-2 ; j>=0 ; j--){
-           
-            ans[j] *= temp ;
-            temp *= nums[j];
-            
+        int forward_multiply[] = new int[nums.length];
+        int backwards_multiply[] = new int[nums.length];
+        forward_multiply[0]=nums[0];
+        backwards_multiply[nums.length-1]=nums[nums.length-1];
+        int ans[] = new int[nums.length];
+        Arrays.fill(ans,1);
+        for(int i = 1 ; i <nums.length ; i++)
+            forward_multiply[i]=nums[i]*forward_multiply[i-1];
+        for(int i = nums.length-2 ; i >=0 ; i--)
+            backwards_multiply[i]=nums[i]*backwards_multiply[i+1];
+        for(int i = 0 ; i <nums.length ; i++){
+            if(i+1<nums.length) ans[i]=backwards_multiply[i+1];
+            if ((i-1)>=0)          ans[i]=ans[i]*forward_multiply[i-1];
         }
         return ans;
     }
-    
-    
-    
 }
