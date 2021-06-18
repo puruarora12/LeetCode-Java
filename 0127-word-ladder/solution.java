f1cd2f8ac290df62d88373cea7d9ad4878410556
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-06-18 20:46:42 +0000

    LeetCode Sync Runtime - 57 ms (89.04%), Memory - 40.5 MB (100.00%)

--- 0127-word-ladder/solution.java
+++ 0127-word-ladder/solution.java
@@ -3,11 +3,9 @@ class Solution {
         Queue<String> que = new LinkedList<>();
         que.add(beginWord);
         int level =1 ; 
-        //int size = 0 ; 
-        
+          
         HashSet<String> hs = new HashSet<>(wordList);
-        //for(String e : wordList) System.out.println(e);
-        
+       
         if (!hs.contains(endWord)) return 0;
         if(hs.contains(beginWord)) hs.remove(beginWord); 
         
@@ -16,42 +14,22 @@ class Solution {
             for(int size = 0 ; size<sizeofque;size++){
             String word = que.poll();
             char wordar[] = word.toCharArray();
-                boolean similar =false;
-                /////////
-                System.out.println();
-                for(char e: wordar) System.out.print(e+" ");
-                System.out.println();
-                /////////
+               
                 for(int index =  0 ; index<wordar.length  ; index++){  
-                            wordar=word.toCharArray();
-                    //if(wordar[index]==endWord.charAt(index)) continue;
+                    wordar=word.toCharArray();                  
                     for(char i = 'a' ; i<='z' ;i++){
-                        if(word.charAt(index)!=i){ 
-                        //System.out.println("word index "+word.charAt(index)+" and 'i' "+i+"  ");
-                            wordar[index]= i ; 
-                         }
-                       String newword = String.valueOf(wordar);
-                        //////////
-                      //  System.out.print(newword+"   ");
-                      //  System.out.print(hs.contains(newword));
-                       // System.out.println();
-                        /////////
-                        
+                        if(word.charAt(index)==i) continue;
+                        wordar[index]= i ; 
+                        String newword = String.valueOf(wordar);
                         if(hs.contains(newword)){
-                        if(newword.equals(endWord)) return level+1;
+                            if(newword.equals(endWord)) return level+1;
                             que.add(newword);
-                            hs.remove(newword);
-                            similar=true;
-                          //  System.out.println("word is "+newword);
-                           // for(String e:hs) System.out.println("elements in set   "+e);
-                          ///  System.out.println();
-                           // for(String e:que) System.out.println("elements in queue   "+e);
+                            hs.remove(newword);                        
                         }
                     }
-                        
-                } /// loop to try each char replacement value
-        }
-                        level++;
+                } 
+            }
+        level++;
         }
         return 0;
     }
