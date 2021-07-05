6d9753faba60679ea252cb5877f4e66a4e1d73d8
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-05 18:14:30 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 39.8 MB (100.00%)

--- 0566-reshape-the-matrix/solution.java
+++ 0566-reshape-the-matrix/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int m  = mat.length;
-        int n = mat[0].length ;
-        if(r==m && n==c) return mat;
-        int nmat[][] = new int[r][c];
-        int oindex =0 ;
-        int nindex =0;
-        if(r*c !=m*n ) return mat;
-        while(oindex <m*n  && nindex<r*c){
-            nmat[nindex/c][(nindex%c)] = mat[oindex/n][oindex%n];
-            oindex++;
-            nindex++;
+       int m = mat.length ; 
+        int n = mat[0].length;
+        if (m*n!=r*c) return mat;
+        if (r==m && c==n) return mat;
+        int n_index = 0 ;
+        int o_index = 0 ;
+        int n_mat[][] = new int[r][c];
+        while(n_index<r*c && o_index<m*n){
+            n_mat[n_index/c][n_index%c] = mat[o_index/n][o_index%n];
+            n_index++;
+            o_index++;
         }
-        return nmat;
+        return n_mat;
     }
 }
