27f4ccbf0587bf398983245f8272cacdf42855db
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-06 17:30:24 +0000

    LeetCode Sync Runtime - 8 ms (95.55%), Memory - 48.7 MB (100.00%)

--- 1464-reduce-array-size-to-the-half/solution.java
+++ 1464-reduce-array-size-to-the-half/solution.java
@@ -1,33 +1,24 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        ArrayList<Integer> ar = new ArrayList<>();
-        int count =0 ;
-        Arrays.sort(arr);
-        int pos  = arr[0];
-        for (int i = 0 ; i<arr.length ; i++){
-            if (pos == arr[i])
-                count++;
-            else{
-                ar.add(count);
-                count =1 ;
-                pos  = arr[i];
-            }
-        }
-        ar.add(count);
-        Collections.sort(ar);
-        for(int e :ar)
-            System.out.print(e+" ");
-        System.out.println();
-        int n = arr.length;
-        int index = ar.size()-1;
-        int setsize = 0;
-        while(n>arr.length/2){
-             n-= ar.get(index);
-            index--;
-            setsize++;
-        }
+        int n = arr.length ;
+        int max = Integer.MIN_VALUE;
+        for(int e :arr) max = Math.max(max ,e);
+        int freq[] = new int[max+1];
         
-        return setsize;
+        for(int e:arr){
+            freq[e]++;
+            
+        }
+        Arrays.sort(freq);
+        
+        int i = max;
+        int count = 0;
+        while(n>arr.length/2){
+            n-=freq[i];
+            i--;
+            count++;
+        }
+        return count;
         
     }
 }
