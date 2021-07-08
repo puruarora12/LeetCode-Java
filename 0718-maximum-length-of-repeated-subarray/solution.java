c690ae82fefb7fdc5b39c55e90248d83c88e7507
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-08 17:52:37 +0000

    LeetCode Sync Runtime - 80 ms (16.58%), Memory - 98.7 MB (5.34%)

--- 0718-maximum-length-of-repeated-subarray/solution.java
+++ 0718-maximum-length-of-repeated-subarray/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int max = 0 ;
-        int dp[][] = new int[nums1.length+1][nums2.length+1];
+        int dp[][] = new int[nums1.length +1][nums2.length+1];
         for(int i = 0 ; i<nums1.length ; i++){
             for(int j = 0 ; j <nums2.length ; j++){
                 if (nums1[i]==nums2[j]){
-                    dp[i+1][j+1] = dp[i][j]+1 ;
-                    max=Math.max(dp[i+1][j+1] ,max);
+                    dp[i+1][j+1] = 1+dp[i][j];
+                    max = Math.max(dp[i+1][j+1] , max);
                 }
             }
         }
