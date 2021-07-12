b63aed50c7fc9ae2301ba3ef281e64cc01b7bfc8
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-12 17:50:30 +0000

    LeetCode Sync Runtime - 2 ms (99.20%), Memory - 39 MB (100.00%)

--- 0205-isomorphic-strings/solution.java
+++ 0205-isomorphic-strings/solution.java
@@ -2,15 +2,14 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         char alpha[] = new char[257];
         boolean ar[] = new boolean[257];
-        char longs[] = s.toCharArray();
-        char shorts[] = t.toCharArray();
-        for(int i  =0 ; i <longs.length ; i++){
-            if (alpha[shorts[i]]==0 && ar[longs[i]]==false ) {
-                alpha[shorts[i]] = longs[i]; 
-                ar[longs[i]]=true;
-                                                                                  }
-            else if  (alpha[shorts[i]]!=longs[i]) return false;
-         
+        char ars[] = s.toCharArray();
+        char art[] = t.toCharArray();
+        
+        for(int i = 0 ; i<ars.length ; i++){
+             if(alpha[art[i]]==0 && ar[ars[i]]==false) {
+                 alpha[art[i]]=ars[i];
+                 ar[ars[i]]=true;
+             }else if (alpha[art[i]]!=ars[i]) return false;
         }
         return true;
     }
