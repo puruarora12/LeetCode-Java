ffa053f71edc82a1c044825e3f6533e2f6dd3bdf
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-12 12:37:35 +0000

    LeetCode Sync Runtime - 5 ms (97.66%), Memory - 39.7 MB (100.00%)

--- 0205-isomorphic-strings/solution.java
+++ 0205-isomorphic-strings/solution.java
@@ -2,22 +2,16 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         char alpha[] = new char[257];
         boolean ar[] = new boolean[257];
-        Arrays.fill(ar , true);
-        Arrays.fill(alpha,' ');
-        String longs = s.length()>t.length() ? s : t;
-        String shorts = s.length()<=t.length() ? s : t;
-       // System.out.println("long is "+longs+"  shots is "+shorts);
+        String longs = s;
+        String shorts = t;
         for(int i  =0 ; i <longs.length() ; i++){
-            if (alpha[shorts.charAt(i)]==' ' && ar[longs.charAt(i)]==true ) {
+            if (alpha[shorts.charAt(i)]==0 && ar[longs.charAt(i)]==false ) {
                 alpha[shorts.charAt(i)] = longs.charAt(i); 
-                ar[longs.charAt(i)]=false;
+                ar[longs.charAt(i)]=true;
                                                                                   }
             else if  (alpha[shorts.charAt(i)]!=longs.charAt(i)) return false;
-            //else if  (ar[longs.charAt(i)-97]==false) return false;
-            else continue;
+         
         }
-     //  for(int i = 0 ; i <26 ; i ++)
-          // System.out.println(alpha[i]+"   "+i);
         return true;
     }
 }
