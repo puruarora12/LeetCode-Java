0431ee4e096e4143109b6d0e0ea1725bb5333eaa
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-13 19:02:29 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 38.4 MB (100.00%)

---
+++ 0162-find-peak-element/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left =0 ;
+        int right = nums.length-1;
+        int mid;
+        while(left<right){
+            mid = left+ (right-left)/2;
+            if (nums[mid]>nums[mid+1]){
+                right=mid;
+            }
+            else left=mid+1;
+        }
+        return left;
+    }
+}
