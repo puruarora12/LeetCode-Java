01637ddccaaeaf84106887381c353e0fbb30f4be
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-14 17:38:09 +0000

    LeetCode Sync Runtime - 3 ms (36.77%), Memory - 37.2 MB (100.00%)

--- 0807-custom-sort-string/solution.java
+++ 0807-custom-sort-string/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public String customSortString(String order, String str) {
+      char alpha[] = new char[26];
+        for(int i = 0 ; i <str.length() ; i++)
+            alpha[str.charAt(i)-97]++;
+        
         String ans = "";
-        char str2[] = str.toCharArray();
-        for (int i = 0 ; i <order.length() ; i++){
-            for(int j = 0 ; j<str.length() ; j++){
-                if (order.charAt(i)==str2[j]) {
-                    ans+=str2[j];
-                    str2[j]=0;                    
-                }
-            }         
+        for(int i = 0 ; i<order.length();i++){
+            ans+=(""+order.charAt(i)).repeat(alpha[order.charAt(i)-97]);
+           alpha[order.charAt(i)-97] =0 ; 
         }
-        Arrays.sort(str2);
-        for(int i =0 ; i <str2.length ; i++)
-            if (str2[i]!=0)ans+=str2[i];
+        
+        for(int i = 0 ;  i<26 ; i++){
+            ans+= (""+(char)(i+97)).repeat(alpha[i]);
+        }
         return ans;
     }
 }
