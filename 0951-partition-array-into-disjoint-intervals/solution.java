002966bf990bfbbe5122c0f723a0020c8665af44
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-07-22 20:14:17 +0000

    LeetCode Sync Runtime - 358 ms (7.29%), Memory - 44.2 MB (100.00%)

--- 0951-partition-array-into-disjoint-intervals/solution.java
+++ 0951-partition-array-into-disjoint-intervals/solution.java
@@ -1,52 +1,25 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
-        int min = searchmin(nums);
-        int lastpos = 0 ; 
-        for(int i = 0 ;  i <nums.length ;i++){
-            if (nums[i]==min)
-                lastpos = i ;
+        int max[] = new int[nums.length];
+        int min[] = new int[nums.length];
+        max[0] = nums[0];
+        for(int i = 1 ; i <nums.length ; i++){
+            max[i] = max[i-1] > nums[i-1] ? max[i-1] : nums[i-1];   
+            System.out.print(max[i]+" ");
         }
+       System.out.println(); 
+        min[nums.length-1] = nums[nums.length-1];
+        for(int i = nums.length-2 ; i>=0 ; i--){
+            min[i] = min[i+1]>nums[i+1]? nums[i+1] : min[i+1];
+        //System.out.print(min[i]+" ");
+        }
+        for(int i :min) System.out.print(i+" ");
         
-        
-       
-        //int ans = lastpos;
-        int ans = 0;
-        int start =0;
-       // System.out.println("min "+min+" lastpos "+lastpos+"  maxinlist "+maxinlist);
-        
-        while(true){
-        int maxinlist = maxelement(nums,start, lastpos);
-        if(maxinlist==nums[lastpos]) return 1;
-         ans= returnindex(nums,lastpos, maxinlist );
-         if(maxelement(nums , lastpos , ans)>maxinlist){
-             start=lastpos;
-             lastpos =ans;
-            continue;
-         }
-        else break;
+        for(int i = 0 ; i<nums.length  ; i++){
+            if(max[i]<=min[i])
+                return i+1 ; 
+            else continue;
         }
-        return ans+1;
-    }
-    public int returnindex(int[] nums , int lastpos , int maxinlist){
-        int temp = lastpos ;  
-        for(int i =lastpos ; i<nums.length ; i++){
-            if(nums[i]<maxinlist)
-                temp = i;
-        }
-        return temp;
-    }
-    public int maxelement(int[] nums ,int start, int index ){
-        int temp = Integer.MIN_VALUE;
-        for(int i =  start ; i <=index ; i++){
-            if (nums[i]>temp)
-                temp = nums[i];
-        }
-        return temp;
-    }
-    
-    public int searchmin(int[] nums){
-        int ar[] = nums.clone();
-        Arrays.sort(ar);
-        return ar[0];
+    return 1;
     }
 }
