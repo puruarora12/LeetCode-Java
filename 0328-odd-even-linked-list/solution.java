cbbd395396d71b1d8ea4e044ed64ca4055839378
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2021-12-02 15:49:11 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 38.4 MB (100.00%)

--- 0328-odd-even-linked-list/solution.java
+++ 0328-odd-even-linked-list/solution.java
@@ -10,19 +10,18 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head==null || head.next==null) return head;
+       if(head==null || head.next==null) return head;
         ListNode odd = head;
-        ListNode even= head.next;
+        ListNode even = head.next;
         ListNode evenhead = even;
-        
         while(even!=null && even.next!=null){
-            odd.next= even.next;
-            even.next = even.next.next;
-            odd=odd.next;
+            odd.next = even.next;
+            even.next=  even.next.next ;
+            odd =odd.next;
             even=even.next;
-            
         }
         odd.next = evenhead;
         return head;
+        
     }
 }
