93225ceba72df68e047173c070f0341831b2e372
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-07-19 19:42:48 +0000

    LeetCode Sync Runtime - 145 ms (6.31%), Memory - 118.6 MB (5.66%)

--- 1014-k-closest-points-to-origin/solution.java
+++ 1014-k-closest-points-to-origin/solution.java
@@ -1,10 +1,6 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[] distance = new int[points.length];
-        
-        for(int i =0 ; i<points.length ; i++) distance[i] = (int)(Math.pow(points[i][0],2)+ Math.pow(points[i][1],2));
-        
-        sort(0 , distance.length-1, distance, points);
+        sort(0 , points.length-1, points);
         int[][] ans = new int[k][2];
         
         
@@ -16,33 +12,30 @@ class Solution {
         return ans;
     }
     
-    public void sort(int left, int right, int[] dis , int[][] points){
+    public void sort(int left, int right, int[][] points){
         if(left<right){
         int mid = left+(right-left)/2;
             //System.out.println("sort "+" left "+left+" mid "+mid+" right "+right);
-            sort(left, mid, dis, points);
-            sort(mid+1, right, dis, points);
-            merge (left, mid , right , dis, points);
+            sort(left, mid, points);
+            sort(mid+1, right, points);
+            merge (left, mid , right, points);
         }
     }
     
-    public void merge(int left , int mid, int right, int[] dis, int[][] points){
+    public void merge(int left , int mid, int right, int[][] points){
         int llength = mid-left+1;
         int rlength = right - mid;
         //System.out.println("merge "+" left "+left+" mid "+mid+" right "+right);
-        int[] ldis = new int[llength];
-        int[] rdis = new int[rlength];
+        
         int[][] lpoints = new int[llength][2];
         int[][] rpoints = new int[rlength][2];
         
         for(int i = 0 ; i<llength ; i++) {
-            ldis[i] = dis[left + i];
-            lpoints[i] = points[left+i];
+           lpoints[i] = points[left+i];
            // System.out.println("lpoints "+lpoints[i][0]+" "+lpoints[i][1]);
         }
         
         for(int i = 0 ; i<rlength ; i++) {
-            rdis[i] = dis[mid+1 + i];
             rpoints[i] = points[mid+1+i];
            //System.out.println("rpoints "+rpoints[i][0]+" "+rpoints[i][1]);
             
@@ -52,12 +45,10 @@ class Solution {
         int lindex =0 ; 
         int rindex = 0;
         while(lindex<llength && rindex<rlength ){
-            if(ldis[lindex]<=rdis[rindex]){
-                dis[index]=ldis[lindex];
+            if(distance(lpoints[lindex])<=distance(rpoints[rindex])){
                 points[index]=lpoints[lindex];
                 lindex++;
             }else{
-                 dis[index]=rdis[rindex];
                 points[index]=rpoints[rindex];
                 rindex++;
             }
@@ -69,18 +60,19 @@ class Solution {
         
         
         while(lindex<llength){
-            dis[index]=ldis[lindex];
-             points[index]=lpoints[lindex];
+            points[index]=lpoints[lindex];
             index++;
             lindex++;
         }
         
         while(rindex<rlength){
-             dis[index]=rdis[rindex];
                 points[index]=rpoints[rindex];
                 index++;
             rindex++;
         }
         
+    }
+    public int distance(int[] point){
+        return (point[0]*point[0]+point[1]*point[1]);
     }
 }
