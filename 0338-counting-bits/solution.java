6207d1c800790c0267d037ff9cb317921cf3c960
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-07-23 13:16:50 +0000

    LeetCode Sync Runtime - 2 ms (94.90%), Memory - 48.6 MB (7.51%)

--- 0338-counting-bits/solution.java
+++ 0338-counting-bits/solution.java
@@ -2,19 +2,20 @@ class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
         ans[0]=0;
-        int count = -1 ;
-        //R System.out.println(15%2);
-        //Q System.out.println(15/2);
+        int count = 1 ;
+        
         
         for(int i =1 ; i <=n ; i++){
-           // System.out.println("i "+i +" count "+count);
-            if(i%(Math.pow(2, count))==0){ 
+            if (i==1) {
                 ans[i]=1;
-                count++;
-            }else{
-                ans[i]= ans[i%((int)(Math.pow(2,count)))]+1;
+            continue;
             }
-          //  if(i>Math.pow(2, count)) count++;
+            if(i%count==0){ 
+                ans[i]=1;
+                count*=2;
+            }else{
+                ans[i]= ans[i%count]+1;
+            }
         }
         return ans;
     }
