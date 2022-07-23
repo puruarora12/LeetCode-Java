1b56bdb5ee285dcc7f6686c3b2c2badbe170b7c2
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-07-23 19:44:59 +0000

    LeetCode Sync Runtime - 30 ms (7.62%), Memory - 70 MB (5.09%)

--- 0415-add-strings/solution.java
+++ 0415-add-strings/solution.java
@@ -4,27 +4,22 @@ class Solution {
         int i = num1.length()-1;
         int j = num2.length()-1;
         String ans = "";
-        while(i>=0 && j>=0){
-            int temp = Character.getNumericValue(num1.charAt(i)) + Character.getNumericValue(num2.charAt(j))+carry;
-            ans = temp%10 +ans;
+        while(i>=0 || j>=0 || carry==1){
+        int n1 = 0;
+        int n2= 0;
+            if(i>-1) {
+                n1 = num1.charAt(i) -'0';
+                i--;
+            }
+            if(j>-1) {
+                n2 = num2.charAt(j) -'0';
+                j--;
+            }
+            int temp = n1+n2+carry;
+            ans= temp%10 + ans;
             carry = temp/10;
-            i--;
-            j--;
         }
         
-        while(i>=0){
-           int temp = Character.getNumericValue(num1.charAt(i))+carry;
-            ans = temp%10 +ans;
-            carry = temp/10;
-            i--;
-        }
-            while(j>=0){
-                 int temp = Character.getNumericValue(num2.charAt(j))+carry;
-            ans = temp%10 +ans;
-            carry = temp/10; 
-            j--;
-            }
-         if(carry==1)ans=1+ans;
         return ans;
     }
 }
