7e86f8a22943ec72b7243759fb545d0c96cf6d52
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-07-24 16:02:09 +0000

    LeetCode Sync Runtime - 3 ms (5.52%), Memory - 41.4 MB (15.57%)

--- 0007-reverse-integer/solution.java
+++ 0007-reverse-integer/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public int reverse(int x) {
-        boolean b = false;
-        if(x<0) b= true;
-        String nums = ""+x;
-        String s  = "";
-        
-        //StringBuilder sb = new StringBuilder();
-        for(int i =0 ; i<nums.length(); i++){
-            if(i==0 && nums.charAt(i)=='-' ) b=true;
-            else s=nums.charAt(i)+s ;
+        long num = 0 ;
+        while(Math.abs(x)>0){
+            num = num*10 + x%10;
+            x/=10;
         }
-        long num = Long.parseLong(s);
-       // System.out.println(num);
-        if(b) num*=-1;
-        if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE) return 0;
-        else return (int)num;
+        if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE)
+            return 0;
+        return (int)num;
     }
 }
