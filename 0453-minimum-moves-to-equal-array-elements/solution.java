3760d44997fb7ece7c332f500a17f2cfd1032f93
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-07-27 16:06:03 +0000

    LeetCode Sync Runtime - 3 ms (35.72%), Memory - 54.8 MB (5.05%)

--- 0453-minimum-moves-to-equal-array-elements/solution.java
+++ 0453-minimum-moves-to-equal-array-elements/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int max =Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         for(int i = 0 ; i<nums.length ; i++){
-            max = Math.max(max , nums[i]);
+            //max = Math.max(max , nums[i]);
             min = Math.min(min, nums[i]);
             
         }
