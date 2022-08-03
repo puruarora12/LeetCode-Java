513ea6b00ab774daea8afd3aaf9fa6d95f015591
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-08-03 19:05:58 +0000

    LeetCode Sync Runtime - 145 ms (9.12%), Memory - 54.9 MB (6.10%)

--- 0729-my-calendar-i/solution.java
+++ 0729-my-calendar-i/solution.java
@@ -9,13 +9,13 @@ class MyCalendar {
     public boolean book(int s, int e) {
       //System.out.println("processing "+s+"  "+e);
         for(int i = 0 ; i<start.size() ; i++){
-            if((s>=start.get(i) && e<=end.get(i)) || 
+           /* if((s>=start.get(i) && e<=end.get(i)) || 
                (e>start.get(i)  && e<end.get(i))  || 
                (s>=start.get(i) && s<end.get(i)) || 
                (s<=start.get(i) && e>start.get(i))
               )  return false; 
-            
-            /*if((s>=start.get(i) && s<end.get(i)) || (e>=start.get(i))  && e<=end.get(i)   ) return false; */
+            */
+            if((s>=start.get(i) && s<end.get(i)) || (e>start.get(i))  && e<=end.get(i) || (s<=start.get(i) && e>end.get(i)) ) return false;    
         }
         //System.out.println("                    added "+s+"   "+e);
         start.add(s); end.add(e);
