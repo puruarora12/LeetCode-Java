aa325175aa985ea7e8806afec31f3e40bc667882
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-18 21:58:06 +0000

    LeetCode Sync Runtime - 2 ms (15.06%), Memory - 52.1 MB (7.50%)

--- 0724-find-pivot-index/solution.java
+++ 0724-find-pivot-index/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-      int sum = 0 ; 
-        for(int i:nums) sum+=i;
-        int leftsum = 0;
-        for(int i =0 ; i<nums.length;i++){
-            if(leftsum==sum-nums[i]-leftsum) return i;
-            leftsum+=nums[i];
+        int sum=0;
+        for (int i :nums) sum+=i;
+        int leftsum=0;
+        for(int i = 0 ; i<nums.length ; i++){
+            if (sum-nums[i]-leftsum==leftsum) return i;
+            else leftsum+=nums[i];
         }
         return -1;
     }
