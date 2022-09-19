78df20a75224558bde17dc8fc371d1b6fbc63054
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-19 22:09:31 +0000

    LeetCode Sync Runtime - 1010 ms (5.19%), Memory - 436 MB (5.27%)

--- 0205-isomorphic-strings/solution.java
+++ 0205-isomorphic-strings/solution.java
@@ -6,25 +6,16 @@ class Solution {
         
         for(int i = 0 ; i<s.length() ; i++ ){
            if(map.containsKey(s.charAt(i))){
-             
-                s=s.substring(0,i)+map.get(s.charAt(i)) +s.substring(i+1 , s.length())  ;;
-              
-              // System.out.println("s is "+s);
+                s=s.substring(0,i)+map.get(s.charAt(i)) +s.substring(i+1 , s.length())  ;
            } 
             else if (ar.contains(t.charAt(i))) return false;
             else  {
                 map.put(s.charAt(i) , t.charAt(i));
-                 s=s.substring(0,i)+map.get(s.charAt(i))+s.substring(i+1 , s.length())  ;;
+                 s=s.substring(0,i)+map.get(s.charAt(i))+s.substring(i+1 , s.length())  ;
                 ar.add(t.charAt(i));
-             
-            }
-          // System.out.println("length of s "+s.length());
-           // System.out.println("s "+s+"  t "+t);
-        }
-        
-        
+            }  
+        } 
     return s.equals(t) ? true: false;
     }
 }
 
-// d -> b and c-> a while b->b and a-> a esi li fail because b and a are already mapped to themselves, need to reverse the order of key value
