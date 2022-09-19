12468cc80f944d3377b813a1843542ed35a40ae3
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-19 22:07:38 +0000

    LeetCode Sync Runtime - 1219 ms (5.19%), Memory - 581.4 MB (5.27%)

--- 0205-isomorphic-strings/solution.java
+++ 0205-isomorphic-strings/solution.java
@@ -1,17 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        char alpha[] = new char[257];
-        boolean ar[] = new boolean[257];
-        char ss[] = s.toCharArray();
-        char tt[] = t.toCharArray();
+        if(s.length()!=t.length()) return false;
+        ArrayList<Character> ar = new ArrayList<>();
+        HashMap<Character, Character> map = new HashMap<>();
         
-        for(int i = 0 ; i<ss.length ; i++){
-            if(alpha[tt[i]]==0 && ar[ss[i]]==false){
-                alpha[tt[i]]=ss[i];
-                ar[ss[i]]=true;
-            }else if (alpha[tt[i]]!=ss[i]) return false;
+        for(int i = 0 ; i<s.length() ; i++ ){
+           if(map.containsKey(s.charAt(i))){
+             
+                s=s.substring(0,i)+map.get(s.charAt(i)) +s.substring(i+1 , s.length())  ;;
+              
+              // System.out.println("s is "+s);
+           } 
+            else if (ar.contains(t.charAt(i))) return false;
+            else  {
+                map.put(s.charAt(i) , t.charAt(i));
+                 s=s.substring(0,i)+map.get(s.charAt(i))+s.substring(i+1 , s.length())  ;;
+                ar.add(t.charAt(i));
+             
+            }
+          // System.out.println("length of s "+s.length());
+           // System.out.println("s "+s+"  t "+t);
         }
-        return true;
         
+        
+    return s.equals(t) ? true: false;
     }
 }
+
+// d -> b and c-> a while b->b and a-> a esi li fail because b and a are already mapped to themselves, need to reverse the order of key value
