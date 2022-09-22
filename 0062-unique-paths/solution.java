33f1348d9444b29d236f0d4df405182d114fc454
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-22 22:31:49 +0000

    LeetCode Sync Runtime - 1 ms (8.33%), Memory - 40.9 MB (17.51%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int[] i:dp)Arrays.fill(i,-1);
-        return path(0,0,m,n,dp);
+        for(int[] i:dp) Arrays.fill(i,-1);
+        return path(0 ,0 , m , n , dp);
     }
     
-    public int path(int i, int j , int m, int n, int[][] dp){
-        if(i==(m-1) && j==(n-1)) return 1;
-        if(i>=m || j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        else return dp[i][j]= path(i+1,j,m,n,dp)+path(i,j+1,m,n,dp);
+    private int path(int i , int j , int m , int n , int[][] dp){
+        if(i==m-1 && j==n-1) return 1; 
+        else if(i>=m || j>=n ) return 0;
+        else if(dp[i][j]!=-1) return dp[i][j];
+        else return dp[i][j]= path(i+1, j , m , n , dp)+ path(i , j+1  ,m , n , dp);
     }
 }
