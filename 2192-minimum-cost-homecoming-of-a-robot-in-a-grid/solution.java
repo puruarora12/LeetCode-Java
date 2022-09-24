f8a11412f5693d4a6985845b72df103d7cdf3559
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-24 19:42:44 +0000

    LeetCode Sync Runtime - 7 ms (6.62%), Memory - 126.3 MB (5.96%)

--- 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/solution.java
+++ 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minCost(int[] startPos, int[] homePos, int[] rowCosts, int[] colCosts) {
-        int cost = 0 , i = startPos[0] , j  = startPos[1] , x = homePos[0] ,  y  = homePos[1] ;
-        while(i!=x){
-            i+=(x-i)/ Math.abs(x-i);
-            cost+=rowCosts[i];
-        }
-        while(j!=y){
-            j+=(y-j)/ Math.abs(y-j);
-            cost+=colCosts[j];
-        }
+        int cost = 0 ;//, i = startPos[0] , j  = startPos[1] , x = homePos[0] ,  y  = homePos[1] ;
+        
+        if(homePos[0]<startPos[0]) while(startPos[0]!=homePos[0]){ startPos[0]-=1 ; cost+=rowCosts[startPos[0]] ; }
+        
+         else if(homePos[0]>startPos[0]) while(startPos[0]!=homePos[0]){ startPos[0]+=1 ; cost+=rowCosts[startPos[0]] ; }
+        
+         if(homePos[1]<startPos[1]) while(startPos[1]!=homePos[1]){ startPos[1]-=1 ; cost+=colCosts[startPos[1]] ; }
+        
+         else if(homePos[1]>startPos[1]) while(startPos[1]!=homePos[1]){ startPos[1]+=1 ; cost+=colCosts[startPos[1]] ; }
+         
         return cost;
     }
 }
