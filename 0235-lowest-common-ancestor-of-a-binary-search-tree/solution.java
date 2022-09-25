54a582a807149c0d4c345f7f8dcddf184dcfc6ca
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-25 22:49:25 +0000

    LeetCode Sync Runtime - 18 ms (1.43%), Memory - 50.4 MB (5.09%)

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.java
@@ -10,26 +10,11 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null) return null;
-        ArrayList<TreeNode> arp = new ArrayList<TreeNode>();
-        ArrayList<TreeNode> arq = new ArrayList<TreeNode>();
-        arp.add(root);
-        arq.add(root);
-        findNode(root, p ,arp) ; 
-        findNode(root, q, arq) ;
-        return checkNode(arp, arq);
-    }
-    
-    private void findNode(TreeNode root ,TreeNode target , ArrayList<TreeNode> ar){
-        if(root==target) return;
-        if(target.val>root.val) {ar.add(root.right) ;  findNode(root.right , target , ar) ;}
-        if(target.val<root.val) {ar.add(root.left) ;  findNode(root.left , target , ar) ;}
-        
-    }
-    
-    private TreeNode checkNode(ArrayList<TreeNode> left , ArrayList<TreeNode> right){
-        int i = 0 ; 
-        while(i<left.size()  && i<right.size() && left.get(i)==right.get(i)) i++;
-        return left.get(i-1);
+        while(root!=null){
+            if(root.val>p.val && root.val > q.val) root=root.left;
+            else if(root.val<p.val && root.val < q.val) root=root.right;
+            else break;
+        }
+        return root;
     }
 }
