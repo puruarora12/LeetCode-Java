aa6683e9c38ea4227d1e856850414be65eb17465
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-09-29 13:10:44 +0000

    LeetCode Sync Runtime - 1 ms (38.53%), Memory - 43.4 MB (37.56%)

---
+++ 0747-min-cost-climbing-stairs/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        
+        for(int i =2 ; i<cost.length ; i++){
+            dp[i] = Math.min(cost[i]+dp[i-1] , cost[i]+dp[i-2]);
+        }
+        dp[cost.length ] = Math.min(dp[cost.length-1] , dp[cost.length-2]);
+        return dp[cost.length];
+    }
+}
