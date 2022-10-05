f31990c0f493332c45fbcfdde7f7ada595151f15
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-10-05 00:38:44 +0000

    LeetCode Sync Runtime - 7 ms (30.62%), Memory - 50.7 MB (5.78%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,9 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0 ; i<nums.length-1 ; i+=2){
-            if(nums[i]!=nums[i+1]) return nums[i];
-        }
-        return nums[nums.length-1];
+        int ans = 0 ; 
+        for(int i :nums)ans^=i;
+        return ans;
     }
 }
