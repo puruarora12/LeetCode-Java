32b4a126c5e775a25a372cbbae71c80a81509fa8
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-10-30 09:25:02 +0000

    LeetCode Sync Runtime - 2 ms (31.81%), Memory - 44 MB (7.89%)

--- 0090-subsets-ii/solution.java
+++ 0090-subsets-ii/solution.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        generate(0 ,nums ,ans , new ArrayList<Integer>() );
+        Arrays.sort(nums);
+        generate(0 ,nums ,ans , new ArrayList<Integer>() , false );
         return ans;
     }
     
-    private void generate(int index  , int[] nums , List<List<Integer>> ans,  List<Integer> ar){
+    private void generate(int index  , int[] nums , List<List<Integer>> ans,  List<Integer> ar , boolean choosen){
         if(index >=nums.length){
-            ArrayList<Integer> arr = new ArrayList<>(ar);
-            Collections.sort(arr);
-            if(!ans.contains(arr))
-            ans.add(arr);
+            
+            //if(!ans.contains(ar))
+            ans.add(new ArrayList<Integer>(ar));
             return ;
         }
+        generate(index + 1 , nums ,ans , ar ,false);
+        if(index-1>=0 && nums[index]==nums[index-1] && !choosen) return;
         
         ar.add(nums[index]);
-        generate(index + 1 , nums ,ans , ar);
-        
+        generate(index + 1 , nums ,ans , ar,true);
         ar.remove(ar.size()-1);
-        generate(index + 1 , nums ,ans , ar);
         
     }
 }
