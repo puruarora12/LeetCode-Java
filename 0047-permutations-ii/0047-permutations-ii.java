13e1a7c307e6418f1d284c533d3cd1daa515b93c
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-10-31 00:07:23 +0530

    Time: 1410 ms (5.01%), Space: 48.1 MB (55.65%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] check = new boolean[nums.length];
+        Arrays.fill(check , true);
+        findPermutes(0 , nums , ans , new ArrayList<>()  ,check );
+        return ans;
+    }
+    
+    private void findPermutes(int index, int[] nums, List<List<Integer>> ans , List<Integer> ar , boolean[] check){
+        if(ar.size()==nums.length){
+            if(!ans.contains(ar))
+            ans.add(new ArrayList<>(ar));
+            return;
+        }
+        
+        for(int i = 0 ; i<nums.length ; i++){
+            if(check[i]){
+                ar.add(nums[i]);
+                check[i]=false;
+                findPermutes(i , nums, ans , ar ,check);
+                ar.remove(ar.size()-1);
+                check[i]=true;
+            }
+        }
+    }
+}
