736998bf6855cd8a731c9c90a2311d92d749fe8a
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-10-31 19:28:39 +0000

    LeetCode Sync Runtime - 15 ms (14.43%), Memory - 47.8 MB (5.92%)

--- 0003-longest-substring-without-repeating-characters/solution.java
+++ 0003-longest-substring-without-repeating-characters/solution.java
@@ -6,29 +6,20 @@ class Solution {
         
         for(int i =0 ; i<s.length();i++){
             int localsize =0;
-           //System.out.println(s.charAt(i));
             if(!map.containsKey(s.charAt(i))){
-            //    System.out.println("in if");
                 map.put(s.charAt(i) , 1);
                 q.add(s.charAt(i));
-             //   System.out.println(q);
             }else{
-              //  System.out.println("in else");
                 char a ;
                 do{
-                    
                     a = q.poll();
-                   // System.out.println("the letter is "+a);
-                   // System.out.println(q);
-                map.remove(a);
+                   map.remove(a);
                 }while(a!=s.charAt(i));
                 q.add(s.charAt(i));
                 map.put(s.charAt(i) , 1);
-           // System.out.println("after else "+q);
-            }
+              }
             localsize = q.size();
-         //   System.out.println(s.charAt(i)+" size "+localsize+" index "+i);
-            if(localsize>size)
+           if(localsize>size)
                 size=localsize;
         }
         return size;
