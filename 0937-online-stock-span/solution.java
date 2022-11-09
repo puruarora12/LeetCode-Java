2f6c6a3390d1ca8efc601d2a629671e13c1b5d5b
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2022-11-09 22:57:15 +0000

    LeetCode Sync Runtime - 58 ms (10.41%), Memory - 75.9 MB (6.41%)

--- 0937-online-stock-span/solution.java
+++ 0937-online-stock-span/solution.java
@@ -5,32 +5,17 @@ class StockSpanner {
     public StockSpanner() {
         ar = new ArrayList<>();
         track = new ArrayList<>();
-        //track.add(null);
     }
     
     public int next(int price) {
         int sum  =1 ;
         if(ar.size()>=1 && ar.get(ar.size()-1)<=price) {
-            //sum += track.get(track.size()-1);
-            int i = track.size()-sum+1-1;
-            //System.out.println();
-            //System.out.println("size: "+track.size()+ "  sum: "+sum + "  i: "+i+" element: "+price );
+            int i = track.size()-sum;
+            
             while(i>=0 && ar.get(i)<=price ){
-                //System.out.println("in while");
-                if(track.get(i)==1){
-                    i--;
-                    sum+=1;
-                    //System.out.println("in if at element "+ar.get(i+1)+" sum:  "+sum+" track: "+track.get(i+1)+" i: "+i);
-                    continue;
-                }
-                 //System.out.println("before at element "+ar.get(i)+" sum:  "+sum+" track: "+track.get(i)+" i: "+i);
-                if(track.get(i)!=1) {
-                    sum+=track.get(i);
-                   // System.out.println("after at element "+ar.get(i)+" sum:  "+sum+" track: "+track.get(i)+" i: "+i);
-                }
+                sum+=track.get(i);
                 i=i-track.get(i);
-               // System.out.println(i);
-            }
+                }
             }
         track.add(sum);
         ar.add(price);
