e93dc6fac5829e19d31b18108e63f3b38253f770
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-01-02 16:06:55 +0000

    LeetCode Sync Runtime - 3 ms (14.55%), Memory - 43 MB (99.98%)

--- 0142-linked-list-cycle-ii/solution.java
+++ 0142-linked-list-cycle-ii/solution.java
@@ -11,22 +11,13 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode fast =head;
-        ListNode slow =head;
-        
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            
-            if(slow==fast) return findNode(head,fast);
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+        while(temp!=null){
+            if(set.contains(temp)) return temp;
+            set.add(temp);
+            temp=temp.next;
         }
         return null;
-    }
-    private static ListNode findNode(ListNode head, ListNode fast){
-        while(head!=fast){
-            head=head.next;
-            fast=fast.next;
-        }
-        return head;
     }
 }
