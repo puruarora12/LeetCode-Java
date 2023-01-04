409ce12287b0ec888dcf2ff05796ecd9eb784398
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-01-04 16:15:15 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 39.6 MB (99.99%)

--- 0908-middle-of-the-linked-list/solution.java
+++ 0908-middle-of-the-linked-list/solution.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow  =head;
-        ListNode fast  =head;
+        ListNode slow = head;
+        ListNode fast= head;
         
         while(fast!=null && fast.next!=null){
             slow =slow.next;
