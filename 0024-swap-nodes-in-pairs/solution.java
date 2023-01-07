0d9bdda96a609d98fadbeb8ccbf97de0f02ebee6
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-01-07 16:47:52 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 40.2 MB (99.98%)

--- 0024-swap-nodes-in-pairs/solution.java
+++ 0024-swap-nodes-in-pairs/solution.java
@@ -10,21 +10,19 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode temp = new ListNode(0);
-        temp.next =head;
-        ListNode returnhead  = temp ;
+     ListNode temp = new ListNode(0);   
+        temp.next = head;
+        ListNode returnhead = temp;
         
-        while(temp.next!=null && temp.next.next !=null){
-            ListNode curr = temp.next ;
-            ListNode nex = temp.next.next;
+        while(temp.next!=null && temp.next.next!=null){
+            ListNode curr = temp.next;
+            ListNode nex = temp.next.next ;
             temp.next = nex;
-            curr.next = nex.next;
+            curr.next = nex.next ;
             nex.next = curr;
             temp = curr;
-            
-            
-            
         }
+        
         return returnhead.next;
     }
 }
