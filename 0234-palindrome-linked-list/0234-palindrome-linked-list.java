68ab66d0d36bf783c996874ffe4d8025b2542554
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-01-08 19:33:46 +0530

    Time: 979 ms (5.08%), Space: 63.2 MB (59.53%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow  =head;
+        ListNode fast =head;
+        Stack<ListNode> stack = new Stack<>();
+        if(head==null || head.next==null ) return true;
+            
+        while(fast!=null && fast.next!=null){
+            stack.push(slow);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        for(ListNode i : stack) System.out.println(i.val);
+        System.out.println("slow.val is "+slow.val);
+        Stack<ListNode> s1 =(Stack<ListNode>)stack.clone();
+        Stack<ListNode> s2 =(Stack<ListNode>)stack.clone();
+       return (checkPalindrome(slow, s1) ||  checkPalindrome(slow.next ,s2)); 
+    }
+    
+    public static boolean checkPalindrome(ListNode slow , Stack<ListNode> stack ){
+        //if(flag==0) stack.pop(); 
+        while(slow!=null && stack.size()!=0){
+            System.out.println(slow.val +"        "+stack.peek().val);
+           if(slow.val!=stack.pop().val) return false;
+        slow=slow.next;
+        }
+        
+       if(slow==null && stack.size()==0) return true; else return false;
+    }
+}
