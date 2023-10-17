55ff9787ff86b290ba2b6b1ccad6a1c1fb955b43
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-10-17 04:48:53 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 38.9 MB (100.00%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,11 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0]=1;
-        dp[1]=2;
-        for(int i =2 ; i<n ; i++ ){
-            dp[i]=dp[i-1]+dp[i-2];
+        int previous = 1;
+        int previous2 = 2;
+        if(n==1 ) return 1;
+        if(n==2) return 2;
+        int total =0;
+        for(int i =3 ; i<=n ; i++ ){
+            total = previous+ previous2;
+            previous = previous2;
+            previous2 =total;
         }
-        return dp[n-1];
+        return total;
     }
+        
 }
