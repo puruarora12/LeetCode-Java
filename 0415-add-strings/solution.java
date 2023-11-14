b479ea7ea5b9efe971a38a18321ee2d82b8685aa
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-11-14 22:40:34 +0000

    LeetCode Sync Runtime - 41 ms (7.62%), Memory - 45.3 MB (7.58%)

--- 0415-add-strings/solution.java
+++ 0415-add-strings/solution.java
@@ -1,25 +1,63 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int carry = 0;
-        int i = num1.length()-1;
-        int j = num2.length()-1;
+        boolean carry = false;
+        
+        int len1 = num1.length()-1;
+        int len2 = num2.length()-1;
         String ans = "";
-        while(i>=0 || j>=0 || carry==1){
-        int n1 = 0;
-        int n2= 0;
-            if(i>-1) {
-                n1 = num1.charAt(i) -'0';
-                i--;
+        while(len1>=0  && len2>=0 ){
+            
+            int n1 = Integer.parseInt(num1.charAt(len1) +"");
+            int n2 = Integer.parseInt(num2.charAt(len2) +"");
+            System.out.println(n1 + " "+n2);
+            int n3 = n2+n1;
+            if (carry){ 
+                n3++ ; 
+                carry =false;
             }
-            if(j>-1) {
-                n2 = num2.charAt(j) -'0';
-                j--;
+            if(n3>=10){
+                carry = true;
+                n3 = n3%10;
             }
-            int temp = n1+n2+carry;
-            ans= temp%10 + ans;
-            carry = temp/10;
+            ans = n3 +ans; 
+            
+            len1--;
+            len2--;
         }
         
+        System.out.println(ans);
+        while(len1>=0){
+            int n = 0;
+            if(carry) {
+                n =(Integer.parseInt(num1.charAt(len1) +"")+1 ) ;
+                carry = false;
+                if(n>=10) {
+                    carry = true;
+                    n = n%10;
+                }
+            }
+            else n = Integer.parseInt(num1.charAt(len1) +"");
+            
+            ans = n +ans;
+            len1--;
+        }
+        
+                while(len2>=0){
+           int n = 0;
+            if(carry) {
+                n =(Integer.parseInt(num2.charAt(len2) +"")+1 ) ;
+                carry = false;
+                if(n>=10) {
+                    carry = true;
+                    n = n%10;
+                }
+            }
+            else n = Integer.parseInt(num2.charAt(len2) +"");
+            
+            ans = n +ans;
+            len2--;
+        }
+        if(carry) ans = 1+ans;
         return ans;
     }
 }
