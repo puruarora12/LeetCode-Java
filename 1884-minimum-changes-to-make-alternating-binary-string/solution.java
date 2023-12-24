56313d3732c98c53469d54267610a9c370958f32
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-12-24 04:06:20 +0000

    LeetCode Sync Runtime - 4 ms (68.26%), Memory - 42.4 MB (53.20%)

--- 1884-minimum-changes-to-make-alternating-binary-string/solution.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/solution.java
@@ -1,27 +1,27 @@
 class Solution {
     public int minOperations(String s) {
         int slen = s.length();
-        String o1 = "";
-        String I1 = "";
-        for (int i = 0 ;i<slen ; i++){
-            if(i%2==0){
-                o1+="1";
-                // I1+="0";
+//         String o1 = "";
+//         String I1 = "";
+//         for (int i = 0 ;i<slen ; i++){
+//             if(i%2==0){
+//                 o1+="1";
+//                 // I1+="0";
                 
-            }
-            else{
-                o1+="0";
-                // I1+="1";
+//             }
+//             else{
+//                 o1+="0";
+//                 // I1+="1";
                 
-            }
+//             }
             
-        }
+//         }
         
         int diff0 = 0;
         // int diff1 = 0;
         
         for(int i = 0;i<s.length() ; i++){
-            if(s.charAt(i)!=o1.charAt(i)) diff0++;
+            if(s.charAt(i)-'0'!=i%2) diff0++;
             // if(s.charAt(i)!=I1.charAt(i)) diff1++;
         }
         return Math.min(diff0 , slen-diff0);
