c5d94504406d9c4bebd1901ec3d0af4dc02b8865
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2023-12-24 04:04:12 +0000

    LeetCode Sync Runtime - 36 ms (5.25%), Memory - 45.4 MB (5.02%)

--- 1884-minimum-changes-to-make-alternating-binary-string/solution.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/solution.java
@@ -6,24 +6,24 @@ class Solution {
         for (int i = 0 ;i<slen ; i++){
             if(i%2==0){
                 o1+="1";
-                I1+="0";
+                // I1+="0";
                 
             }
             else{
                 o1+="0";
-                I1+="1";
+                // I1+="1";
                 
             }
             
         }
         
         int diff0 = 0;
-        int diff1 = 0;
+        // int diff1 = 0;
         
         for(int i = 0;i<s.length() ; i++){
             if(s.charAt(i)!=o1.charAt(i)) diff0++;
-            if(s.charAt(i)!=I1.charAt(i)) diff1++;
+            // if(s.charAt(i)!=I1.charAt(i)) diff1++;
         }
-        return Math.min(diff0 , diff1);
+        return Math.min(diff0 , slen-diff0);
     }
 }
