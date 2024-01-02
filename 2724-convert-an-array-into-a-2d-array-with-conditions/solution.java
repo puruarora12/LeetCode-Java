65d6a3730d146d169cef5f88276ea186e057cd3c
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-01-02 22:33:40 +0000

    LeetCode Sync Runtime - 7 ms (7.73%), Memory - 45 MB (71.46%)

--- 2724-convert-an-array-into-a-2d-array-with-conditions/solution.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/solution.java
@@ -4,28 +4,13 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         ans.add(new ArrayList<Integer>(Arrays.asList(nums[0])));
         
-        //System.out.println("After first insert "+ans.size()+"   "+ans.get(0));
+        
         int row = 2;
         for(int i = 1 ; i <nums.length  ;i++){
             if(nums[i]!=nums[i-1]) row = 1;
             
-            if(row>ans.size()){
-                //System.out.println("in if row>ans "+row+"  "+ans.size()+" the element is "+nums[i]);
-                ans.add(new ArrayList<Integer>(Arrays.asList(nums[i])));
-                //System.out.println("ans size after "+ans.size());
-            }else{
-                
-                ans.get(row-1).add(nums[i]);
-                //System.out.println("adding the element "+nums[i]);
-            }
-            
-            
-            //for (List<Integer> z : ans){
-            //    System.out.println(z);
-            //}
-            //System.out.println();
-            //System.out.println(ans.size());
-            
+            if(row>ans.size()) ans.add(new ArrayList<Integer>(Arrays.asList(nums[i])));
+            else ans.get(row-1).add(nums[i]);     
             row++;
         }
         
