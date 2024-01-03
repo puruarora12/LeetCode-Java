d5661e2f4d961f123e09377a745e783b379fe337
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-01-03 05:42:03 +0000

    LeetCode Sync Runtime - 11 ms (31.95%), Memory - 47.3 MB (98.78%)

--- 0049-group-anagrams/solution.java
+++ 0049-group-anagrams/solution.java
@@ -9,29 +9,22 @@ class Solution {
            for(int j = 0 ; j<strs[i].length() ;j++ )
                chars[strs[i].charAt(j)-'a']++;
             String svalue = String.valueOf(chars);
-            //System.out.println(svalue);
-                     if(map.containsKey(String.valueOf(chars))){
-                //System.out.println(" int map contains "+chars);
+            if(map.containsKey(svalue)){
+                 
+                ans.get(map.get(svalue)).add(strs[i]);
                
-                
-                ans.get(map.get(String.valueOf(chars))).add(strs[i]);
-                //words.add(strs[i]);
-                //map.put(pos , words);
-                
             }else{
                 List<String> words = new ArrayList<>();
                 words.add(strs[i]);
                 ans.add(words);
-                map.put(String.valueOf(chars), index);
+                map.put(svalue, index);
                 index++; 
-              //  System.out.println("the index is "+index);
+              
             }
             
             
           
         }  
-       // for(int[] i : map.keySet())ans.add(map.get(i)) ; 
-        
         return ans;
     }
     
