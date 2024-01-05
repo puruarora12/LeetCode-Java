f5a606c4c16932ab285ec6a33addf77a9f8aa520
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-01-05 03:45:39 +0000

    LeetCode Sync Runtime - 5 ms (49.53%), Memory - 44.7 MB (44.78%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,14 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //String ans = "";
-        s=s.toLowerCase();
         ArrayList<Character> ar = new ArrayList<>();
-        for(char i : s.toCharArray())
-            if((i>='0' && i<='9') || (i>='a' && i<='z') ||(i>='A' && i<='Z') ) 
-                ar.add(i);
+        //s.replace(" " , "");
+        //s.lowerCase();
+        for(int i = 0 ; i<s.length() ; i++){
+            //System.out.println(s.charAt(i));
+            if(s.charAt(i)>='A'&& s.charAt(i)<='Z'){
+                ar.add((char)(s.charAt(i)+32));
+                
+            }else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                ar.add(s.charAt(i));
+            }else if(s.charAt(i)>='0' && s.charAt(i)<='9') ar.add(s.charAt(i));
+            else continue;
+            //System.out.println(ar);
+        }
         
-        ArrayList<Character> arr = new ArrayList<>(ar);
-        Collections.reverse(arr);
-        return arr.equals(ar);
+        ArrayList<Character> ar2 = new ArrayList<>(ar);
+        Collections.reverse(ar);
+        //System.out.println(ar);
+        //System.out.println(ar2);
+        if(ar.equals(ar2)) return true;
+        else return false;
+        
     }
 }
