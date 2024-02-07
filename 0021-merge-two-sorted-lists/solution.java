1767ddbfbbd7a5fc7c0f5e36a4fdf0219c858d06
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-02-07 03:49:17 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 42.4 MB (74.47%)

--- 0021-merge-two-sorted-lists/solution.java
+++ 0021-merge-two-sorted-lists/solution.java
@@ -10,41 +10,45 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head ; 
-        if(list1!=null && list2!=null && list1.val<list2.val){
-            head =list1;
-            list1 = list1.next;
-        }
-        else if(list1!=null && list2!=null && list1.val>=list2.val) {
-            head= list2;
+        if(list1 == null) return list2;
+        if(list2==null) return list1;
+        
+        ListNode head ;
+        if(list1.val<list2.val){
+            head= list1;
+            list1=list1.next;
+        }else{
+            head = list2;
             list2 =list2.next;
+        
         }
-        else if (list1==null) return list2;
-        else return list1;
-        ListNode temp ;
-        ListNode toreturn = head;
+        ListNode ans = head;
+        //head=head.next;
         
         while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                head.next = list1;
-                head =head.next;
-                list1=list1.next;
-            }else{
-                head.next = list2;
-                head =head.next;
-                list2=list2.next;  
-            }
+                if(list1.val<list2.val){
+            head.next= list1;
+            list1=list1.next;
+        }else{
+            head.next = list2;
+            list2 =list2.next;
+        
         }
+            head=head.next;            
+        }
+        
         while(list1!=null){
-            head.next = list1;
-                head =head.next;
-                list1=list1.next;
+                
+            head.next= list1;
+            list1=list1.next;
+            head=head.next;            
         }
         while(list2!=null){
-            head.next = list2;
-                head =head.next;
-                list2=list2.next;
+                
+            head.next= list2;
+            list2=list2.next;
+            head=head.next;            
         }
-        return toreturn;
+        return ans;
     }
 }
