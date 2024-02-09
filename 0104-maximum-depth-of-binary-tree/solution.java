687416c6e9c50cee83f058ed3e0f3443644a1f28
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-02-09 01:59:54 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 42.5 MB (88.00%)

--- 0104-maximum-depth-of-binary-tree/solution.java
+++ 0104-maximum-depth-of-binary-tree/solution.java
@@ -15,15 +15,14 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        int depth = 1 ;
-        return check(root , depth);
-        
+        if(root==null)return 0;
+        int depth = 1;
+        return check(root, depth);
     }
-    private int check(TreeNode root , int depth){
-        if(root.left==null && root.right==null) return depth;
-        else if (root.right==null) return depth = check(root.left , depth+1);
-        else if (root.left==null) return depth = check(root.right , depth+1);
-        else return depth = Math.max(check(root.left , depth+1) , check(root.right , depth+1));
+    public static int check(TreeNode root, int depth){
+        if(root.left==null && root.right ==null ) return depth;
+        else if(root.left==null) return check(root.right, depth+1);
+        else if (root.right==null) return check(root.left , depth+1);
+        else return Math.max(check(root.left , depth+1 ) ,check(root.right , depth+1));
     }
 }
