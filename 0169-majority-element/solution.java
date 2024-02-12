2a53f806f645558e468b4f5e2071df9d36ba2fe4
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-02-12 23:35:36 +0000

    LeetCode Sync Runtime - 7 ms (38.48%), Memory - 50.5 MB (70.44%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0 ;
-         int majority =0 ; 
-        for(int i =0 ; i <nums.length ; i++){
-            if(count==0 )majority=nums[i];
-            if(majority==nums[i])count++;
-            else count --;
+        Arrays.sort(nums);
+        for(int i = 0 ; i<nums.length- nums.length/2 ; i++){
+            //System.out.println(i+"   "+(i+(int)Math.floor(nums.length/2)));
+            if(nums[i]==nums[i+(int)Math.floor(nums.length/2)]) return nums[i];
         }
-        return majority;
+        return -1;
     }
 }
