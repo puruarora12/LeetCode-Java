8d55beb933593ffb83ee601342de558ce86290af
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-02-14 09:52:59 +0000

    LeetCode Sync Runtime - 3 ms (100.00%), Memory - 76.8 MB (76.22%)

--- 2271-rearrange-array-elements-by-sign/solution.java
+++ 2271-rearrange-array-elements-by-sign/solution.java
@@ -1,37 +1,15 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int ans[] = new int[nums.length];
-        int index = 0;
-        int pos  = 0;
-        int neg = 0;
-        
-        while(pos<nums.length && neg<nums.length){
-            if(index%2==0 && nums[pos]>=0){
-                ans[index]=nums[pos];
-                index++;
-                pos++;
-            }
-            else if(index%2==0 && nums[pos]<0)pos++;
-            else if(index%2!=0 && nums[neg]<0){
-                ans[index]=nums[neg];
-                index++;
-                neg++;
-            }else if(index%2!=0 && nums[neg]>0) neg++;
-            else continue;
-        }
-       // System.out.println("index "+index+"  pos "+pos+"   neg "+neg);
-        if(index<nums.length){
-            if(pos<nums.length){
-                while(nums[pos]<0)pos++;
-                ans[index]=nums[pos];
-                index++;
-                pos++;
-            }
-            else {
-                while(nums[neg]>0)neg++;
-                ans[index]=nums[neg];
-                index++;
-                neg++;
+        int pos = 0 ;
+        int neg = 1; 
+        for(int i = 0 ; i<nums.length ; i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos+=2;
+            }else{
+                ans[neg]=nums[i];
+                neg+=2;
             }
         }
         return ans;
