822bf09eb3f879d80ea41f92454f7c1cecdb6efd
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-02-28 08:35:51 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 42.5 MB (100.00%)

--- 0513-find-bottom-left-tree-value/solution.java
+++ 0513-find-bottom-left-tree-value/solution.java
@@ -20,18 +20,14 @@ class Solution {
        
         level=0;
         toreturn=root;
-        bfs(root, 0, true) ;
+        bfs(root, 0) ;
         return toreturn.val;
     }
     
-    public static void bfs(TreeNode root, int depth, boolean isleft){
-        Queue<TreeNode> q  = new LinkedList<>();
-        //TreeNode toreturn=root;
-        //System.out.println(root.val+"    "+isleft+"    "+depth);
+    public static void bfs(TreeNode root, int depth){
         if(root.left==null && root.right==null)  if(depth>level ){level=depth; toreturn= root;}
-        if(root.left!=null) bfs(root.left, depth+1, true);
-        if(root.right!=null) bfs(root.right, depth+1, false);
-        //return toreturn;
-        //else return Math.max(bfs(root.left, depth+1, true) , bfs(root.right, depth+1, false));
+        if(root.left!=null) bfs(root.left, depth+1);
+        if(root.right!=null) bfs(root.right, depth+1);
+       
     }
 }
