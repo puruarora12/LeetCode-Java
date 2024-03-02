c2c986677cc78b9394c487f54bd638bd18578198
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-03-02 00:10:07 +0000

    LeetCode Sync Runtime - 7 ms (44.44%), Memory - 46.9 MB (71.12%)

--- 1019-squares-of-a-sorted-array/solution.java
+++ 1019-squares-of-a-sorted-array/solution.java
@@ -1,18 +1,9 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] ans = new int[nums.length];
-        int i = 0 ; 
-        int j = nums.length - 1;
-        for(int index = nums.length-1 ; index>=0 ; index-- ){
-            if(Math.abs(nums[i])>Math.abs(nums[j])){
-                ans[index] =nums[i]*nums[i];
-                i++;
-                    
-            }else{
-                ans[index] =nums[j]*nums[j];
-                j--;
-            }
+        for(int i = 0 ; i<nums.length ; i++){
+            nums[i]*=nums[i];
         }
-        return ans;
+        Arrays.sort(nums);
+        return nums;
     }
 }
