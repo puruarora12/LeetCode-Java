50364700a140e39aa20452160cbb324f1bd13ba0
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-03-05 01:15:33 +0000

    LeetCode Sync Runtime - 2 ms (100.00%), Memory - 43.1 MB (29.38%)

--- 0985-bag-of-tokens/solution.java
+++ 0985-bag-of-tokens/solution.java
@@ -1,18 +1,17 @@
 class Solution {
-      public int bagOfTokensScore(int[] A, int P) {
-        Arrays.sort(A);
-        int res = 0, points = 0, i = 0, j = A.length - 1;
-        while (i <= j) {
-            if (P >= A[i]) {
-                P -= A[i++];
-                res = Math.max(res, ++points);
-            } else if (points > 0) {
-                points--;
-                P += A[j--];
-            } else {
-                break;
-            }
+    public int bagOfTokensScore(int[] tokens, int power) {
+       Arrays.sort(tokens);
+        int left = 0; 
+        int right = tokens.length-1 ;
+        int score=0;
+        while(left<=right){
+           //System.out.println("start score "+score+" left "+tokens[left] +"  right "+tokens[right]+ "   power "+power);
+            if(left>right)break;  
+            if(power>=tokens[left]) { power-=tokens[left]; score++; left++;}
+           else if(power<tokens[left] && left<right && power+tokens[right]>tokens[left] && score>0){score--; power+=tokens[right]; right--; }
+            else return score;
+             //System.out.println("After score "+score+" left "+tokens[left] +"  right "+tokens[right]+ "   power "+power);
         }
-        return res;
+        return score;
     }
 }
