db54011e2da4b3f0a3108ddeb856cd9048d22ba9
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-03-08 03:57:14 +0000

    LeetCode Sync Runtime - 3 ms (28.23%), Memory - 42.2 MB (70.88%)

--- 3242-count-elements-with-maximum-frequency/solution.java
+++ 3242-count-elements-with-maximum-frequency/solution.java
@@ -1,25 +1,17 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        //Arrays.sort(nums);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int total = 0 ;
-        for(int i = 0 ;i<nums.length ; i++){
-            map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
-            }
+        int freq[] = new int[101];
+        for(int i : nums) freq[i]++;
+        Arrays.sort(freq);
+        int index = 0;
+        //while(freq[index]==0) index++;
+        int max = 1;
+        int ans = 0;
+        for(int i = 99 ; i>=0 ; i--){
+            if(freq[i]==freq[i+1]) max++;
+            else break;
+        }
         
-        ArrayList<Integer> ar= new ArrayList<>();
-        for(int i :map.values()){
-        ar.add(i);
-           // System.out.println(i);
-        }
-        Collections.sort(ar);
-        System.out.println(ar);
-        for(int i = ar.size()-1 ; i>=0; i--){
-            if(ar.get(i)==ar.get(ar.size()-1))
-            total+=ar.get(i);
-            
-        }
-        return total;
-        }
+        return max*freq[100];
     }
-
+}
