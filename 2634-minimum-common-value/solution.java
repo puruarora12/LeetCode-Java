5d5d38919539dfa20a1905af193bc9dc9dbc88bb
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-03-09 21:21:57 +0000

    LeetCode Sync Runtime - 2 ms (84.10%), Memory - 62.2 MB (92.43%)

--- 2634-minimum-common-value/solution.java
+++ 2634-minimum-common-value/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int index1 = 0 ; 
-        int index2 = 0;
-        
-        while(index1<nums1.length && index2 <nums2.length){
-            if(nums1[index1]==nums2[index2] ) return nums1[index1];
-            
-            if(nums1[index1]<nums2[index2])index1++;
-            else index2++;
+        int a1=0;
+        int a2=0;
+        while(a1<nums1.length && a2<nums2.length){
+            if (nums1[a1]==nums2[a2])return nums1[a1];
+            else if (nums1[a1]>nums2[a2])a2++;
+            else a1++;
         }
         return -1;
     }
