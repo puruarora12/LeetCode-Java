b9e07ab454edfb5091280fab2ed3fc53d3c076d7
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-03-11 10:24:52 +0000

    LeetCode Sync Runtime - 7 ms (5.76%), Memory - 41.9 MB (34.77%)

--- 0807-custom-sort-string/solution.java
+++ 0807-custom-sort-string/solution.java
@@ -1,16 +1,31 @@
 class Solution {
-    public String customSortString(String order, String str) {
-        int alpha[] = new int[26];
-        for(int i = 0 ; i <str.length(); i++) alpha[str.charAt(i)-97]++;
+    public String customSortString(String order, String s) {
+        int ar[] = new int[26];
+        Arrays.fill(ar,0);
+        boolean inorder[] = new boolean[26];
+        Arrays.fill(inorder, false);
+        for(int i = 0 ; i<s.length(); i++){
+            ar[s.charAt(i)-'a']++;
+        }
         String ans = "";
+        for(int i = 0 ;i<order.length() ; i++){
+            int index =order.charAt(i)-'a';
+            inorder[index]=true;
+            while(ar[index]>0) {
+                ans+=order.charAt(i);
+                ar[index]--;
+            }
+        }
         
-        for(int i = 0 ; i<order.length(); i++){
-            ans += (""+order.charAt(i)).repeat(alpha[order.charAt(i)-97]);
-            alpha[order.charAt(i)-97] = 0 ;
-        }
         for(int i = 0 ; i<26 ; i++){
-           ans += (""+(char)(i+97)).repeat(alpha[i]);
+            if(inorder[i]==false && ar[i]>0){
+              while(ar[i]>0) {
+                ans+=(char)(i+'a');
+                ar[i]--;
+            }  
+            }
         }
+        
         
         return ans;
     }
