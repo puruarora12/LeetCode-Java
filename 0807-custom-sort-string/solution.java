23e78e40db6a32e3e90377254a2995930e1f7e19
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-03-11 10:26:45 +0000

    LeetCode Sync Runtime - 1 ms (81.99%), Memory - 41.3 MB (97.34%)

--- 0807-custom-sort-string/solution.java
+++ 0807-custom-sort-string/solution.java
@@ -2,17 +2,18 @@ class Solution {
     public String customSortString(String order, String s) {
         int ar[] = new int[26];
         Arrays.fill(ar,0);
+        StringBuilder ans = new StringBuilder();
         boolean inorder[] = new boolean[26];
         Arrays.fill(inorder, false);
         for(int i = 0 ; i<s.length(); i++){
             ar[s.charAt(i)-'a']++;
         }
-        String ans = "";
+        //String ans = "";
         for(int i = 0 ;i<order.length() ; i++){
             int index =order.charAt(i)-'a';
             inorder[index]=true;
             while(ar[index]>0) {
-                ans+=order.charAt(i);
+                ans.append(order.charAt(i));
                 ar[index]--;
             }
         }
@@ -20,13 +21,13 @@ class Solution {
         for(int i = 0 ; i<26 ; i++){
             if(inorder[i]==false && ar[i]>0){
               while(ar[i]>0) {
-                ans+=(char)(i+'a');
+                ans.append((char)(i+'a'));
                 ar[i]--;
             }  
             }
         }
         
         
-        return ans;
+        return ans.toString();
     }
 }
