680f89f5f6b4450dd17130dcf87fee7ee3331149
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-05-28 00:22:46 +0000

    LeetCode Sync Runtime - 2 ms (51.83%), Memory - 41.2 MB (90.66%)

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/solution.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/solution.java
@@ -2,17 +2,10 @@ class Solution {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
         int num = 1 ;
-        //boolean found = false;
         for(int i = nums.length-1 ; i>=0;i--){
-            //System.out.println(nums[i-1] +"   "+num+"   "+nums[i]  );
-            if(i-1>=0 && (nums[i-1]<num && nums[i]>=num)) {
-                //found =true;
-                
-                return num;
-            }else if(i==0){
-                if(nums[i]>=num) return num;
-            }
-                else num++;
+            if(i-1>=0 && (nums[i-1]<num && nums[i]>=num))  return num;
+            else if(i==0 && nums[i]>=num) return num;
+            else num++;
         }
         return -1;
     }
