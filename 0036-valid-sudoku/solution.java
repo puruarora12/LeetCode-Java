d55a6321c4c05319ad5e8cbeeb019eb851a1ca33
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-08-01 04:49:52 +0000

    LeetCode Sync Runtime - 4 ms (38.02%), Memory - 44.2 MB (88.85%)

--- 0036-valid-sudoku/solution.java
+++ 0036-valid-sudoku/solution.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        // 4/7 inspection
         for(int i = 0 ; i<9 ; i++){
             HashSet<Character> setij = new HashSet<>();
             HashSet<Character> setji = new HashSet<>();
@@ -14,18 +15,17 @@ class Solution {
                 else setji.add(board[j][i]);
             }
         }
-        // int r = 0 ;
-        // System.out.println("Till here");
+      
         int c = 0 ;
         int r =0;
         for(int i =0 ; i<3 ; i++){
-           // c=0;
+           
             for(int j =0 ; j<3 ; j++){
                  r=i*3;
                  HashSet<Character> set = new HashSet<>();
                 for(int x =0 ; x<3 ; x++ ){
                      
-                     // System.out.println("j is "+j+" x is "+x+"       "+ r+"  "+c);
+                   
                         if (set.contains(board[r][c])) return false;
                         else if (board[r][c]=='.') ;
                         else set.add(board[r][c]);
@@ -37,15 +37,15 @@ class Solution {
                     if (set.contains(board[r+2][c])) return false;
                         else if (board[r+2][c]=='.') ;
                         else set.add(board[r+2][c]);
-                    // System.out.println("checked "+r+""+c +" "+(r+1)+""+c+" "+(r+2)+""+c);
                     c++;
                    
                 
             }
-                // System.out.println() ;               
+                  
+                           
               
         }
-           // System.out.println("///////////////////////////////////");                   
+                        
 c=0;
         }
             return true;
