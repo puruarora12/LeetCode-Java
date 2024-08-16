d07ffcf5becaf555a26aaabc6740ff68619c600f
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-08-16 09:22:34 +0000

    LeetCode Sync Runtime - 1 ms (99.45%), Memory - 41.9 MB (80.94%)

---
+++ 3379-score-of-a-string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0 ;
+        for(int i = 0 ; i<s.length()-1; i++){
+            sum+=Math.abs((int)s.charAt(i) -(int)s.charAt(i+1) );
+        }
+        return sum;
+    }
+}
