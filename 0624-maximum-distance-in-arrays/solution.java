5e7a48acacec41a704fd2a79e1397e437e0ab100
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-08-16 07:37:29 +0000

    LeetCode Sync Runtime - 63 ms (5.08%), Memory - 69.9 MB (7.23%)

--- 0624-maximum-distance-in-arrays/solution.java
+++ 0624-maximum-distance-in-arrays/solution.java
@@ -52,33 +52,33 @@ class Solution {
         Object[] ma = maxs.keySet().toArray();
         // System.out.println(mi.length);
         int right = ma.length-1;
-        System.out.println(mins.keySet());
-        System.out.println(maxs.keySet());
+        // System.out.println(mins.keySet());
+        // System.out.println(maxs.keySet());
         while(left<=right){
-            System.out.println(left+" "+right);
-            System.out.println("left element "+mi[left]+" index "+mins.get(mi[left])+"    right element "+ma[right]+" index  "+maxs.get(ma[right]));
+            // System.out.println(left+" "+right);
+            // System.out.println("left element "+mi[left]+" index "+mins.get(mi[left])+"    right element "+ma[right]+" index  "+maxs.get(ma[right]));
             if(left<mi.length && right>=0 &&(!mins.get(mi[left]).equals(maxs.get(ma[right])) || (mins.get(mi[left]).size()+maxs.get(ma[right]).size()>=3)))  {
-                System.out.println("in if "); 
+                // System.out.println("in if "); 
                 diff= Math.abs((int)mi[left]-(int)ma[right]); 
                 break;
             }
             else if (mins.get(mi[left]).equals(maxs.get(ma[right])) && left==right ) {
-                System.out.println("in if "); 
+                // System.out.println("in if "); 
                 diff= Math.max(Math.abs((int)mi[left]-(int)ma[right]),diff); 
                 break;
             }
             else if( (int)mi[left+1]- (int)mi[left]<= (int)ma[right]-(int)ma[right-1]) {
-                System.out.println("in left "+( (int)mi[left+1]- (int)mi[left]) +"    "+((int)ma[right]-(int)ma[right-1])) ;
+                // System.out.println("in left "+( (int)mi[left+1]- (int)mi[left]) +"    "+((int)ma[right]-(int)ma[right-1])) ;
                 left++;
             } 
             else if( (int)mi[left+1]- (int)mi[left]>(int)ma[right]-(int)ma[right-1]){
-                System.out.println("in right "+( (int)mi[left+1]- (int)mi[left]) +"    "+((int)ma[right]-(int)ma[right-1])) ;
+                // System.out.println("in right "+( (int)mi[left+1]- (int)mi[left]) +"    "+((int)ma[right]-(int)ma[right-1])) ;
                 right--;
             }
             else break;
             
             
-             System.out.println(left+" "+right);
+             // System.out.println(left+" "+right);
         }
         if(!mins.get(mi[left]).equals(maxs.get(ma[right])))  diff= Math.max(Math.abs((int)mi[left]-(int)ma[right]),diff);
         return diff;
