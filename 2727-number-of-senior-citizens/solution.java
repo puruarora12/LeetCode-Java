0dffe76b9a37dfa0d4f358ed0f589091f0c99cda
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-08-16 08:08:18 +0000

    LeetCode Sync Runtime - 1 ms (77.03%), Memory - 42.6 MB (59.39%)

--- 2727-number-of-senior-citizens/solution.java
+++ 2727-number-of-senior-citizens/solution.java
@@ -1,12 +1,9 @@
 class Solution {
     public int countSeniors(String[] details) {
-       int count =0;
-        for(String a : details){
-            //System.out.println((Integer.parseInt(a.substring(12,14))));
-            if(Integer.parseInt(a.substring(11,13))>60) count++;
+        int count =0;
+        for(String i: details){
+            if(Integer.parseInt(i.substring(11,13))>60)count++;
         }
-    return count;
+        return count;
     }
-    
-  
 }
