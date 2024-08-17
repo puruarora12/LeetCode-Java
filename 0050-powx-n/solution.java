8d8c34f7cf4027de9a0022733924df49cb530d08
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-08-17 08:29:30 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 41.7 MB (93.66%)

--- 0050-powx-n/solution.java
+++ 0050-powx-n/solution.java
@@ -1,21 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans =x;
-        double ox = x ;
-        
-//         int pos = (int)Math.floor(Math.log(Math.abs(n))/Math.log(2.0));
-//         int num = (int)Math.pow(2, pos);
-//         int diff = Math.abs(n)-num;
-//         System.out.println(pos+"   "+num);
-//         while(num>1){
-//             if(n>0) x *=x;
-//             else if (n<0)x/=x;
-//             num=num/2;
-//             System.out.println(x+ "     "+num);
-//         }
-//         System.out.println(diff);
-        
-//         while(diff-->0) x*=ans;
-        return Math.pow(ox, n);
+      
+        return Math.pow(x, n);
     }
 }
