046e0bc10f0aadcd913a9dae384f52dfee4b669a
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-10-02 23:18:19 +0000

    LeetCode Sync Runtime - 24 ms (91.37%), Memory - 62.2 MB (62.35%)

--- 1256-rank-transform-of-an-array/solution.java
+++ 1256-rank-transform-of-an-array/solution.java
@@ -2,22 +2,11 @@ class Solution {
 
     public int[] arrayRankTransform(int[] arr) {
 
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         for(int i :arr){
-//             map.put(i, map.getOrDefault(i, 0)+1);
-//         }
-        
-        
-        
-        
-        // ArrayList<Integer> ar = new ArrayList<>();
-        // for(int i:arr)if(!ar.contains(i))ar.add(i);
-        // Collections.sort(ar);
         int[] ar = Arrays.copyOf(arr,arr.length);
         Arrays.sort(ar);
         HashMap<Integer, Integer> map = new HashMap<>();
         int index =1;
-        // int prev =Integer.MAX_VALUE;
+       
         for(int i :ar){
             if(!map.containsKey(i)){
                 map.put(i, index);
@@ -25,10 +14,7 @@ class Solution {
             }
         }
         int[] ans = new int[arr.length];
-        
-         for(int i =0 ; i<arr.length ; i++)
-            //ans[i]=map.get(arr[i]);
-            ans[i]=map.get(arr[i]);
+        for(int i =0 ; i<arr.length ; i++) ans[i]=map.get(arr[i]);
 
         return ans;
 
