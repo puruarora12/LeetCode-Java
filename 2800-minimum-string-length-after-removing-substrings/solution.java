c30c1b5b78ba80ff22c52dfe417f00db22bf31d4
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2024-10-07 19:43:08 +0000

    LeetCode Sync Runtime - 4 ms (87.92%), Memory - 43.2 MB (78.65%)

--- 2800-minimum-string-length-after-removing-substrings/solution.java
+++ 2800-minimum-string-length-after-removing-substrings/solution.java
@@ -2,15 +2,13 @@ class Solution {
     public int minLength(String t) {
         Stack<Character> s = new Stack<>();
         for(char i :t.toCharArray()){
-            // System.out.println(i);
             if(i!='B' && i!='D')s.push(i);
-            else{
-                // System.out.println("in else ");
-                if(s.size()>0 && (( i=='B' && s.peek()=='A' ) || ( i=='D' && s.peek()=='C'))){ s.pop();
-                // System.out.println("popped");
-                }else s.push(i);
-            }
-            // System.out.println("peeked "+s.peek());
+            else
+           
+                if(s.size()>0 && (( i=='B' && s.peek()=='A' ) || ( i=='D' && s.peek()=='C'))) s.pop();
+                else s.push(i);
+            
+         
         }
         return s.size();
     }
