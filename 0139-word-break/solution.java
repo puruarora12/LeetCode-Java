2bdadee6212d0c1739da3343d57f739ee61b2c80
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-02-25 21:50:18 +0000

    LeetCode Sync Runtime - 6 ms (62.09%), Memory - 41.8 MB (93.43%)

--- 0139-word-break/solution.java
+++ 0139-word-break/solution.java
@@ -4,7 +4,7 @@ class Solution {
         dp[s.length()]=true;
         for(int i = s.length()-1 ; i>=0 ; i--){
             for(String word  : wordDict){
-                if(i+word.length() <=s.length() && s.substring(i,i+word.length()).equals(word) && dp[i+word.length()]==true) dp[i]=true;
+                if(i+word.length() <=s.length() && s.regionMatches(i, word, 0, word.length()) && dp[i+word.length()]==true) dp[i]=true;
             }
         }
         return dp[0];
