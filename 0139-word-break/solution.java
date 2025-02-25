4e6ca2197126600663d80c890ebbec9293b1c423
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-02-25 21:18:00 +0000

    LeetCode Sync Runtime - 8 ms (44.35%), Memory - 43.5 MB (61.22%)

--- 0139-word-break/solution.java
+++ 0139-word-break/solution.java
@@ -4,12 +4,9 @@ class Solution {
         dp[s.length()]=true;
         for(int i = s.length()-1 ; i>=0 ; i--){
             for(String word  : wordDict){
-                // if(i+word.length() <=s.length())
-                // System.out.println("word "+s.substring(i,i+word.length())+ " second word index "+(i+word.length()));
                 if(i+word.length() <=s.length() && s.substring(i,i+word.length()).equals(word) && dp[i+word.length()]==true) dp[i]=true;
             }
         }
-        for(boolean i: dp) System.out.println(i);
         return dp[0];
     }
 }
