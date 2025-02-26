80de259a7bfd7a9bbc004f03e75193b2cb0eb078
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-02-26 01:30:39 +0000

    LeetCode Sync Runtime - 82 ms (5.46%), Memory - 45.6 MB (11.35%)

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        List<Integer> pre = Arrays.stream(preorder).boxed().collect(Collectors.toList());
+        List<Integer> in = Arrays.stream(inorder).boxed().collect(Collectors.toList());
+
+        return makeTree(pre ,in);
+    }
+
+    public static TreeNode makeTree(List<Integer> pre , List<Integer> in){
+        if(pre.size()==0 )return null;
+        if(in.size()==0 )return null;
+        System.out.println(pre.size()+"  "+in.size());
+        TreeNode root = new TreeNode(pre.get(0));
+        System.out.println("pre values "+pre.get(0));
+        int mid = in.indexOf(pre.get(0));
+        System.out.println("mid is "+mid);
+        // System.out.println((in.indexOf(pre.get(0))+1) +"   "+ (pre.indexOf(in.get(0))+1));
+        if (mid>=0){
+        System.out.println("left");
+        root.left = makeTree(pre.subList(1 , mid+1 ) , in.subList(0 , mid+1));
+        
+        }if(mid>=0){
+        System.out.println("right");
+        root.right = makeTree(pre.subList(mid+1 , pre.size())  , in.subList( mid+1, in.size()));
+        }return root;
+    }
+
+
+}
