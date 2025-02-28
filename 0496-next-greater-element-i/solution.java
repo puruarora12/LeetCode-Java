9f5edf7c963afe7a5467008594151320fd61b4fe
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-02-28 06:11:30 +0000

    LeetCode Sync Runtime - 17 ms (6.01%), Memory - 43.3 MB (81.77%)

--- 0496-next-greater-element-i/solution.java
+++ 0496-next-greater-element-i/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        Arrays.fill(ans, -1);
-        int index  =0;
-        while(index<nums1.length){
-            boolean eq =false;
-        for(int i = 0 ; i<nums2.length ; i++){
-            if(nums1[index]==nums2[i]) eq=true;
-            
-            if(eq && nums2[i]>nums1[index]) {ans[index]=nums2[i]; break;}
-            }
-            index++;
+        List<Integer> ls = Arrays.stream(nums2).boxed().collect(Collectors.toList());
+        int ans[] = new int[nums1.length];
+        Arrays.fill(ans,-1);
+        for(int i=0; i<nums1.length ; i++){
+            int pos = ls.indexOf(nums1[i]);
+            for(int j = pos+1 ; j<nums2.length; j++)if(nums2[j]>nums1[i]) {ans[i]=nums2[j];break;}
+
         }
-        return ans;
+      return ans;  
     }
 }
