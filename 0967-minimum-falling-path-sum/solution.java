d3b0f5e53c669a645e44d8ff227bf3454233648f
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-03 06:42:58 +0000

    LeetCode Sync Runtime - 8 ms (34.27%), Memory - 45 MB (89.58%)

--- 0967-minimum-falling-path-sum/solution.java
+++ 0967-minimum-falling-path-sum/solution.java
@@ -2,9 +2,6 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         for(int i =1 ;i<matrix.length ;i++){
             for(int j =0 ; j<matrix.length;j++){
-                // System.out.println(" d "+(j+1)+" ma "+(matrix.length-1));
-                // System.out.println("left d "+Math.max(0,j-1));
-                // System.out.println("right d "+Math.min(matrix.length-1,j+1));
                 matrix[i][j]+=Math.min(matrix[i-1][j], Math.min(matrix[i-1][Math.max(0,j-1)], matrix[i-1][Math.min(j+1, matrix.length-1)] ) );
             }
         }
