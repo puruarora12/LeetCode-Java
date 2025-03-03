0ae867b23353e349690769b317dd02893453e9de
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-03 01:01:48 +0000

    LeetCode Sync Runtime - 18 ms (5.69%), Memory - 42.6 MB (62.43%)

--- 0893-all-nodes-distance-k-in-binary-tree/solution.java
+++ 0893-all-nodes-distance-k-in-binary-tree/solution.java
@@ -8,18 +8,18 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode, TreeNode> map ;
+    // HashMap<TreeNode, TreeNode> map ;
     List<Integer> ans;
-    List<TreeNode> visited;
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         ans= new ArrayList<>();
-        map = new HashMap<>();
-        visited= new ArrayList<>();
-        buildTree(root);
-        findNode(target, k);
+         HashMap<TreeNode, TreeNode> map = new HashMap<>();
+        List<TreeNode> visited= new ArrayList<>();
+        buildTree(root,map);
+        findNode(target, k, visited,map);
         return ans;
     }
-    private void findNode(TreeNode root, int k){
+    private void findNode(TreeNode root, int k,List<TreeNode> visited, HashMap<TreeNode, TreeNode> map){
         visited.add(root);
         // System.out.println("node is "+root.val+" k is "+k);
         if(k==0) { 
@@ -30,17 +30,17 @@ class Solution {
         if(root.left!=null && !visited.contains(root.left)) { 
             // System.out.println("processing left");
             // visited.add(root.left);
-            findNode(root.left, k-1); 
+            findNode(root.left, k-1,visited,map); 
         }
         if(root.right!=null && !visited.contains(root.right)){ 
             // System.out.println("processing right");
             // visited.add(root.right);
-            findNode(root.right, k-1);
+            findNode(root.right, k-1,visited,map);
         }
         if(map.containsKey(root) && !visited.contains(map.get(root))){
             //  System.out.println("processing parent");
             //  visited.add(map.get(root));
-             findNode(map.get(root), k-1);
+             findNode(map.get(root), k-1,visited,map);
         }
         return;
 
@@ -48,14 +48,14 @@ class Solution {
 
 
 
-    private void buildTree(TreeNode root){
+    private void buildTree(TreeNode root, HashMap<TreeNode, TreeNode> map){
         if(root.left!=null){
             map.put(root.left,root);
-            buildTree(root.left);
+            buildTree(root.left,map);
         }
         if(root.right!=null){
             map.put(root.right,root);
-            buildTree(root.right);
+            buildTree(root.right,map);
         }
         // System.out.println(map);
     }
