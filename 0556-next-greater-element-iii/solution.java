300aae8d73d5b15134b74c8542630adced8552b1
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-05 05:43:46 +0000

    LeetCode Sync Runtime - 6 ms (4.95%), Memory - 41.4 MB (8.83%)

--- 0556-next-greater-element-iii/solution.java
+++ 0556-next-greater-element-iii/solution.java
@@ -10,19 +10,19 @@ class Solution {
         
         char[] s = ss.toCharArray();
         int small = s.length-1;
-        // System.out.println(ss);
+    
         for(int i= s.length-1 ; i>0; i--){
-        // System.out.println(s[i-1] + "   "+s[i]+   "   "+(s[i-1]<s[i])  );
+        
             if(s[i]<s[small])small=i;
           if(s[i-1]<s[i]){
             int temp =i;
             small=temp;
             while(temp<s.length ){
-                // System.out.println("finding small "+s[small]+"  from "+s[i-1]+ " currr is "+s[temp]);
+               
                 if(s[temp]>s[i-1] && s[small]>s[temp]) small=temp;
                 temp++;
             } 
-            // System.out.println("found small is "+s[small]);
+           
             char t =s[i-1];
             s[i-1]=s[small];
             s[small]=t;
@@ -32,13 +32,11 @@ class Solution {
           }
         }
         Arrays.sort(s, small, s.length);
-        // System.out.println(s);
+       
         
         long val = Long.parseLong(new String(s));
         if(val==(long) OG) return -1;
-        // for(char i:s) System.out.print(i);
-        // char[] max = ( (Math.pow(2,31)-1)+"").toCharArray();
-        // System.out.println("compare value "+(Arrays.compare(s, max)));
+       
         return val<=Integer.MAX_VALUE ? (int) val :-1;
     }
 }
