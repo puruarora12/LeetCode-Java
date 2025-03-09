a58b1518648f1dbe5f9e403bd7432c54a4a5884b
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-09 01:31:07 +0000

    LeetCode Sync Runtime - 94 ms (72.60%), Memory - 44.6 MB (81.08%)

--- 2610-closest-prime-numbers-in-range/solution.java
+++ 2610-closest-prime-numbers-in-range/solution.java
@@ -5,21 +5,18 @@ class Solution {
         ar[0]=false;
         ar[1]=false;
         for(int i = 2 ; i*i<=right ; i++){
-            // System.out.println("left "+left+ " right "+right+"  i "+ i+ " ar[i-left] "+(i-left) );
             if(ar[i]){
-                // System.out.println("in false loop "+i*i);
+     
                 for(int j = i*i ; j<=right; j+=i)ar[j]=false;
             }
         }
         
-        // for(boolean i:ar) System.out.print(i+" ");
-        // System.out.println();
+
         int l=left;
         int r = l+1; 
         int diff = Integer.MAX_VALUE;
         int ans[] =  new int[]{-1,-1};
         while(l<r && r<=right){
-            // System.out.println(l+"   "+r);
             if(ar[l]==ar[r] && ar[l]==true){
                 if(diff>r-l){
                     diff = r-l;
