40d01872ad64baac49e9be680f2af3f873a1d3b1
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-11 01:13:24 +0000

    LeetCode Sync Runtime - 3 ms (90.11%), Memory - 43.6 MB (63.45%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        
-        int[] ch = new int[26];
-        for(char i :s.toCharArray()) ch[i-'a']++;
-        //int[] ch2 = new int[26];
-        for(char i :t.toCharArray()) ch[i-'a']--;
-       
-        for(int i :ch) if(i!=0) return false;
-        return true;
-       
+        int[] ss = new int[26];
+        int[] tt = new int[26];
+        Arrays.fill(ss,0);
+        Arrays.fill(tt,0);
+        for(char i:s.toCharArray()) ss[i-'a']++;
+        for(char i:t.toCharArray()) tt[i-'a']++;
+        return Arrays.toString(ss).equals(Arrays.toString(tt)) ? true:false;
+
     }
 }
