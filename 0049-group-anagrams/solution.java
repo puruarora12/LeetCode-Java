861f30f0d5f556d4312a8ef6e43461743ae3b2a8
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-11 01:21:36 +0000

    LeetCode Sync Runtime - 19 ms (20.59%), Memory - 49.3 MB (8.39%)

--- 0049-group-anagrams/solution.java
+++ 0049-group-anagrams/solution.java
@@ -1,32 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String , Integer> map = new HashMap<>();
-        Integer index = 0;
+        HashMap<String, List<String>> map= new HashMap<>();
+        for(String s: strs){
+            int[] chars = new int[26];
+            for(char c:s.toCharArray())chars[c-'a']++;
+            List<String> words = new ArrayList<>();
+            String chrs = Arrays.toString(chars);
+            words = map.getOrDefault(chrs, words);
+            words.add(s);
+            map.put(chrs, words);
+        }
         List<List<String>> ans = new ArrayList<>();
-        
-        for (int i = 0 ; i<strs.length ; i++ ){
-           char[] chars = new char[26];
-           for(int j = 0 ; j<strs[i].length() ;j++ )
-               chars[strs[i].charAt(j)-'a']++;
-            String svalue = String.valueOf(chars);
-            if(map.containsKey(svalue)){
-                 
-                ans.get(map.get(svalue)).add(strs[i]);
-               
-            }else{
-                List<String> words = new ArrayList<>();
-                words.add(strs[i]);
-                ans.add(words);
-                map.put(svalue, index);
-                index++; 
-              
-            }
-            
-            
-          
-        }  
+        for(String s:map.keySet()) ans.add(map.get(s));
         return ans;
+
     }
-    
-    
 }
