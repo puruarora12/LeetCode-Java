ba8f7e3116712fa820118d0a614c4a2a78a96348
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-13 00:14:28 +0000

    LeetCode Sync Runtime - 12 ms (75.15%), Memory - 44.8 MB (37.86%)

--- 1460-number-of-substrings-containing-all-three-characters/solution.java
+++ 1460-number-of-substrings-containing-all-three-characters/solution.java
@@ -2,14 +2,16 @@ class Solution {
     public int numberOfSubstrings(String s) {
         int count =0 ; 
         int left = 0  ;
+        char[] ch = s.toCharArray();
+        int len = ch.length;
         int[] freq = new int[3];
 
-        for(int right = 0 ; right<s.length() ;right++){
-            freq[s.charAt(right)-'a']++;
+        for(int right = 0 ; right<len ;right++){
+            freq[ch[right]-'a']++;
 
             while(freq[0]>0 && freq[1]>0 && freq[2]>0){
-                count+=s.length()-right;
-                freq[s.charAt(left)-'a']--;
+                count+=len-right;
+                freq[ch[left]-'a']--;
                 left++;
             }
         }
