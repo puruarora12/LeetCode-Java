b4f85e94b20c6cd241e9a58697d90fc524df6de9
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-13 23:04:48 +0000

    LeetCode Sync Runtime - 502 ms (5.24%), Memory - 48.9 MB (43.33%)

--- 0347-top-k-frequent-elements/solution.java
+++ 0347-top-k-frequent-elements/solution.java
@@ -1,48 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Arrays.sort(nums);
-        TreeMap<Integer, ArrayList<Integer>> mmap = new TreeMap<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int freq =1 ;
-        for(int i = 1 ; i<nums.length ; i++){
-            if(nums[i]==nums[i-1]) freq++;
-            else{
-                map.put(nums[i-1], freq);
-                freq=1;
+        List<int[]> ls = new ArrayList<>();
+        List<Integer> fre = new ArrayList<>();
+        for(int i:nums){
+            if(fre.contains(i)){
+                int index = fre.indexOf(i);
+                ls.get(index)[1]= ls.get(index)[1]+1;
+            }else{
+            fre.add(i);
+            ls.add(new int[]{i,1});
             }
         }
-        map.put(nums[nums.length-1] ,freq);
-       
-        
-        for(int i: map.keySet()){
-            ArrayList<Integer> ar = new ArrayList<>();
-            if(mmap.containsKey(map.get(i))){
-                ar= mmap.get(map.get(i));
-            
-                
-            }
-            ar.add(i);
-            mmap.put(map.get(i),ar);
-          
-        }
-        
-        
-        int ans[] = new int[k];
-        ArrayList<Integer> num = new ArrayList<>();
-        for(int i :mmap.keySet()){
-            for(int index = 0 ; index<mmap.get(i).size() ; index++){
-                num.add(mmap.get(i).get(index));
-            }
-        }
-        int index = 0 ;
-        int pos = num.size()-1;
-        while(k>0){
-            ans[index] = num.get(pos);
-            pos--;
-            index++;
-            k--;
-        }
-        
+        // System.out.println(fre);
+        Collections.sort(ls, (a,b )-> b[1]-a[1]);
+        int[] ans = new int[k];
+        for(int i =0 ; i<k ; i++)ans[i]=ls.get(i)[0];
         return ans;
+
     }
 }
