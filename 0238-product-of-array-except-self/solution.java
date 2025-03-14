b0089072609a576e035024187080a48beacf593e
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-14 00:32:15 +0000

    LeetCode Sync Runtime - 3 ms (21.51%), Memory - 56.5 MB (11.51%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // 3/7 to inspection.
-        int left[] = new int[nums.length];
-        int right[] = new int[nums.length];
-        left[0]=nums[0]; right[nums.length-1]=nums[nums.length-1];
-        
-        for(int i =1 ; i<nums.length; i++) left[i]=left[i-1]*nums[i];
-        for(int i = nums.length-2; i>=0 ; i--) right[i]=right[i+1]*nums[i];
-        
-        int ans[]  = new int[nums.length];
-        
-        ans[0]=right[1]; ans[nums.length-1]=left[nums.length-2];
-        
-        for(int i=1 ; i<=nums.length-2; i++)ans[i]=left[i-1]*right[i+1];
+        int n = nums.length;
+        int ans[] = new int[n];
+        int left[] = new int[n];
+        left[0]=nums[0];
+        int right[]  = new int[n];
+        right[n-1]=nums[n-1];
+        for(int i =1;i<n ; i++)left[i]=nums[i]*left[i-1];
+        for(int i =n-2;i>=0 ; i--)right[i]=nums[i]*right[i+1];
+        for(int i =0;i<n ; i++){
+            if(i==0)ans[0]=right[i+1];
+            else if(i==n-1) ans[i]=left[i-1];
+            else ans[i]=left[i-1]*right[i+1];
+        }
         return ans;
     }
 }
