bebd6c08f5d6f4b4bad236fc2fbd8d86f6cbdbed
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-14 00:34:53 +0000

    LeetCode Sync Runtime - 2 ms (87.85%), Memory - 56.4 MB (11.51%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -8,10 +8,10 @@ class Solution {
         right[n-1]=nums[n-1];
         for(int i =1;i<n ; i++)left[i]=nums[i]*left[i-1];
         for(int i =n-2;i>=0 ; i--)right[i]=nums[i]*right[i+1];
-        for(int i =0;i<n ; i++){
-            if(i==0)ans[0]=right[i+1];
-            else if(i==n-1) ans[i]=left[i-1];
-            else ans[i]=left[i-1]*right[i+1];
+        ans[0]=right[1];
+        ans[n-1]=left[n-2];
+        for(int i =1;i<n-1 ; i++){
+             ans[i]=left[i-1]*right[i+1];
         }
         return ans;
     }
