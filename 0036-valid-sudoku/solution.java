2b5a37c5c46a630ee95fbfabbd2890a9bde9f867
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-14 01:25:25 +0000

    LeetCode Sync Runtime - 2 ms (77.28%), Memory - 44.4 MB (67.34%)

--- 0036-valid-sudoku/solution.java
+++ 0036-valid-sudoku/solution.java
@@ -1,54 +1,38 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // 4/7 inspection
-        for(int i = 0 ; i<9 ; i++){
-            HashSet<Character> setij = new HashSet<>();
-            HashSet<Character> setji = new HashSet<>();
-            for(int j = 0 ; j<9 ; j++){
-                // System.out.println(i +"   "+j);
-                if (setij.contains(board[i][j])) return false;
-                else if (board[i][j]=='.') ;
-                else setij.add(board[i][j]);
-                
-                if (setji.contains(board[j][i]) )return false;
-                else if (board[j][i]=='.') ;
-                else setji.add(board[j][i]);
+        boolean[] found = new boolean[127];
+        
+
+        //rows
+        for(char i[]:board){
+            Arrays.fill(found,false);
+            for(char n:i){
+                if(n!='.' && found[n]) return false;
+                found[n]=true;
+            }   
+        }
+
+        //cols
+        int[][] dirs = new int[][]{{0,0} ,{1,0} , {2,0} , {0,1} ,{1,1} , {2,1} ,{0,2} ,{1,2} , {2,2} };
+        for(int i = 0 ; i<board.length ; i++){
+            Arrays.fill(found,false);
+            for(int j =0 ; j<board.length ; j++){
+                if(board[j][i]!='.' && found[board[j][i]] )return false;
+                found[board[j][i]]=true;
             }
         }
-      
-        int c = 0 ;
-        int r =0;
-        for(int i =0 ; i<3 ; i++){
-           
-            for(int j =0 ; j<3 ; j++){
-                 r=i*3;
-                 HashSet<Character> set = new HashSet<>();
-                for(int x =0 ; x<3 ; x++ ){
-                     
-                   
-                        if (set.contains(board[r][c])) return false;
-                        else if (board[r][c]=='.') ;
-                        else set.add(board[r][c]);
-                    
-                    if (set.contains(board[r+1][c]) )return false;
-                        else if (board[r+1][c]=='.') ;
-                        else set.add(board[r+1][c]);
-                    
-                    if (set.contains(board[r+2][c])) return false;
-                        else if (board[r+2][c]=='.') ;
-                        else set.add(board[r+2][c]);
-                    c++;
-                   
-                
+
+        //diag
+        for(int i =0 ; i<board.length ; i+=3){
+            for(int j =0 ; j<board.length; j+=3){
+                Arrays.fill(found,false);
+                for(int x[] :dirs){
+                    if(board[i+x[0]][j+x[1]]!='.' && found[board[i+x[0]][j+x[1]]] )return false;
+                    found[board[i+x[0]][j+x[1]]]=true;
+                }
             }
-                  
-                           
-              
         }
-                        
-c=0;
-        }
-            return true;
-            
+
+        return true;
     }
 }
