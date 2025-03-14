107b193b2c5181ae837c26f13b565edfa3864607
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-14 04:26:16 +0000

    LeetCode Sync Runtime - 26 ms (91.57%), Memory - 52.8 MB (14.10%)

--- 0015-3sum/solution.java
+++ 0015-3sum/solution.java
@@ -5,18 +5,15 @@ class Solution {
         for(int i =0 ; i<nums.length-2 ; i++){
             if(nums[i]>0) break;
             if(i>0 && nums[i]==nums[i-1])continue;
-            // Set<Integer> set = new HashSet<>();
             int left =i+1;
             int right=nums.length-1;
 
             while(left<right){
                 if(nums[left]+nums[i]+nums[right]==0){
-                         List<Integer> ls = new ArrayList<>();
+                    List<Integer> ls = new ArrayList<>();
                     ls.add(nums[i]);
                     ls.add(nums[left]);
                     ls.add(nums[right]);
-                    // Collections.sort(ls);
-                    // if(!ans.contains(ls))
                     ans.add(ls);
                     while(left<right-1 && nums[right]==nums[right-1] ) right--;
                     while((left+1<right && nums[left]==nums[left+1] ) ) left++;
@@ -25,13 +22,8 @@ class Solution {
 
                 }
                 else if(  nums[left]+nums[i]+nums[right]>0)right--;
-                //else if( || nums[left]+nums[i]+nums[right]<0)left++;
-                else{ left++;
-                   
-                }
-
-                // set.add(nums[index]);
-                // index++;
+                else left++;
+                
             }
         }
         return ans;
