f3ddf0aced2da0a08965c17819fd914bb5fdf971
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-14 04:25:31 +0000

    LeetCode Sync Runtime - 26 ms (91.57%), Memory - 52.9 MB (12.53%)

--- 0015-3sum/solution.java
+++ 0015-3sum/solution.java
@@ -1,32 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        ArrayList<List<Integer>> ar = new ArrayList<>();
-        
-        for(int i  =0  ; i <nums.length-2 ; i++){
+        for(int i =0 ; i<nums.length-2 ; i++){
             if(nums[i]>0) break;
-                if(i== 0 || (i>0 && nums[i]!=nums[i-1])){
-                //var sum = 0-nums[i];
-                var left = i+1 ; 
-                var right = nums.length-1 ;
-                
-                //System.out.println("before loop");
-                while(left<right){
-                   //System.out.println(nums[left]+nums[right]+"    and sum is "+nums[i]);
-                    if(nums[i]+nums[left]+nums[right] ==0){
-                        //System.out.println("in iff in loop");
-                        ar.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
-                        while(left<right && nums[left]==nums[left+1]) left++;
-                        while(left<right && nums[right]==nums[right-1]) right--;
-                        left++; 
-                        right--;
-                    }else if(nums[i]+nums[left]+nums[right]>0) right--;
-                    else left++;
+            if(i>0 && nums[i]==nums[i-1])continue;
+            // Set<Integer> set = new HashSet<>();
+            int left =i+1;
+            int right=nums.length-1;
+
+            while(left<right){
+                if(nums[left]+nums[i]+nums[right]==0){
+                         List<Integer> ls = new ArrayList<>();
+                    ls.add(nums[i]);
+                    ls.add(nums[left]);
+                    ls.add(nums[right]);
+                    // Collections.sort(ls);
+                    // if(!ans.contains(ls))
+                    ans.add(ls);
+                    while(left<right-1 && nums[right]==nums[right-1] ) right--;
+                    while((left+1<right && nums[left]==nums[left+1] ) ) left++;
+                    right--;
+                    left++;
+
                 }
-                
-            }    
+                else if(  nums[left]+nums[i]+nums[right]>0)right--;
+                //else if( || nums[left]+nums[i]+nums[right]<0)left++;
+                else{ left++;
+                   
+                }
+
+                // set.add(nums[index]);
+                // index++;
+            }
         }
-        
-        return ar;
+        return ans;
     }
 }
