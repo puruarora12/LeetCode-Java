01e456083d08fa5eb31812939b927f5e359c00d7
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-19 09:04:32 +0000

    LeetCode Sync Runtime - 110 ms (45.56%), Memory - 64.1 MB (15.39%)

--- 0295-find-median-from-data-stream/solution.java
+++ 0295-find-median-from-data-stream/solution.java
@@ -1,26 +1,28 @@
 class MedianFinder {
-
-   private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
-private PriorityQueue<Integer> large = new PriorityQueue<>();
-private boolean even = true;
-
-public double findMedian() {
-    if (even)
-        return (small.peek() + large.peek()) / 2.0;
-    else
-        return small.peek();
-}
-
-public void addNum(int num) {
-    if (even) {
-        large.offer(num);
-        small.offer(large.poll());
-    } else {
-        small.offer(num);
-        large.offer(small.poll());
+PriorityQueue<Integer> small ;
+PriorityQueue<Integer> large ;
+boolean even;
+    public MedianFinder() {
+        small = new PriorityQueue<>((a,b)->b-a);
+        large= new PriorityQueue<>();
+        even=true;
     }
-    even = !even;
-}
+    
+    public void addNum(int num) {
+        if(even){
+            large.offer(num);
+            small.offer(large.poll());
+        }else{
+            small.offer(num);
+            large.offer(small.poll());            
+        }
+        even=!even;
+    }
+    
+    public double findMedian() {
+        if(even) return (small.peek() +large.peek())/2.0;
+        else return small.peek();
+    }
 }
 
 /**
