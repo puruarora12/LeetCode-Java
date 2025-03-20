5ea397fd4feda0f606c309a87944c2f4b31e06e3
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-20 08:56:43 +0000

    LeetCode Sync Runtime - 1 ms (100.00%), Memory - 44.9 MB (31.56%)

--- 0695-max-area-of-island/solution.java
+++ 0695-max-area-of-island/solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    int max ;
+    
     public int maxAreaOfIsland(int[][] grid) {
-     max = 0 ;
+    int max = 0 ;
         for(int i =0 ; i<grid.length; i++)
             for(int j= 0 ; j<grid[i].length ; j++)
                 if(grid[i][j]==1)
-                    max = Math.max( helper(grid, i, j, 0) ,max);
+                    max = Math.max( helper(grid, i, j) ,max);
      return max ;
 
     }
 
-    private int helper(int[][] grid, int i , int j , int len){
+    private int helper(int[][] grid, int i , int j ){
         if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]==0) return 0;
-        grid[i][j]=0; len++; 
+        grid[i][j]=0; 
        int area = 1;
-        return area +helper(grid, i+1 , j , len)
-        +helper(grid, i-1 , j , len)
-        +helper(grid, i, j+1  , len)
-        +helper(grid, i, j-1  , len);
+        return area +helper(grid, i+1 , j )
+        +helper(grid, i-1 , j )
+        +helper(grid, i, j+1  )
+        +helper(grid, i, j-1 );
         
         
 
