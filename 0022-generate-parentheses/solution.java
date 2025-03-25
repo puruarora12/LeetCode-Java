6acb8f21be66ff24401337a52bb4145d7c2816c2
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-25 20:41:01 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 42.9 MB (82.48%)

--- 0022-generate-parentheses/solution.java
+++ 0022-generate-parentheses/solution.java
@@ -1,17 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        calculate(0 ,0, n , ans , "");
+        dfs(n,n,new StringBuilder(), ans);
         return ans;
     }
-    
-    private void calculate(int open, int closed ,int n , List<String> ans , String ar){
-        if(ar.length()==n*2){
-            ans.add(ar);
+    public void dfs(int start, int end, StringBuilder sb , List<String> ans){
+        if(start==0 && end==0){
+            ans.add(sb.toString());
             return;
+
         }
-        if(open<n)calculate(open+1, closed , n , ans , ar+"(");
-        if(closed<open) calculate(open , closed+1 , n , ans , ar+")");
-           
+
+        if(start>0){
+            sb.append("(");
+            dfs(start-1, end , sb, ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        if(end>start){
+            sb.append(")");
+            dfs(start, end-1 , sb, ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
 }
