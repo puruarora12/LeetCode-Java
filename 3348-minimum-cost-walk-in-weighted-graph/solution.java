9d55f440b87e7f566b40d21057afa623f109e19f
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-26 22:35:23 +0000

    LeetCode Sync Runtime - 8 ms (50.77%), Memory - 125.5 MB (33.80%)

--- 3348-minimum-cost-walk-in-weighted-graph/solution.java
+++ 3348-minimum-cost-walk-in-weighted-graph/solution.java
@@ -24,10 +24,9 @@ class Solution {
         for(int i =0 ; i<query.length; i++){
             int start= query[i][0] ,  end = query[i][1];
 
-            int startRoot = find(parent, start);
-            int endRoot = find(parent, end);
+           
             if(start==end)results[i]=0;
-            else if(startRoot!=endRoot)results[i]=-1;
+            else if(find(parent, start)!=find(parent, end))results[i]=-1;
                 else{
                     results[i]=cost[find(parent, start)];
                 
