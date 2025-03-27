c3654869e99ef18740a028c09bc568a31ca14d62
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-27 07:05:38 +0000

    LeetCode Sync Runtime - 11 ms (92.04%), Memory - 45.7 MB (86.79%)

--- 0347-top-k-frequent-elements/solution.java
+++ 0347-top-k-frequent-elements/solution.java
@@ -1,17 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,int[]>map = new HashMap<>();
-        for(int i:nums){
-            int[] value = map.getOrDefault(i,new int[2]);
-            value[0]=i;
-            value[1]+=1;
-            map.put(i,value);
-        }
+        HashMap<Integer, Integer>map = new HashMap<>();
+        for(int i:nums)map.put(i, map.getOrDefault(i,0)+1);
 
-        List<int[]> ls=new ArrayList<>(map.values());
-        Collections.sort(ls, (a,b)->b[1]-a[1]);
+        ArrayList<Integer>[] bucket= new ArrayList[nums.length+1];
+
+        for(int i:map.keySet()){
+            int freq = map.get(i);
+            if(bucket[freq]==null)bucket[freq]=new ArrayList<>();
+
+            bucket[freq].add(i);
+        }
         int[] ans = new int[k];
-        for(int i =0 ; i<k ; i++)ans[i]=ls.get(i)[0];
+        int index =0 ;
+        for(int i=nums.length; i>=0 && index<k; i--){
+                if(bucket[i]==null)continue;
+                for(int x: bucket[i]){
+                        if(index<k)ans[index++]=x;
+                        else break;
+                        
+                }
+        }
         return ans;
     }
 }
