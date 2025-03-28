6fe9ca7a46251a8d605a3fd5899ad07f17c6a0b1
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-28 23:58:08 +0000

    LeetCode Sync Runtime - 2650 ms (5.01%), Memory - 45.2 MB (13.92%)

---
+++ 0079-word-search/solution.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        Queue<int[]> q = new LinkedList<>();
+        int n =board.length;
+        int m  = board[0].length;
+    
+        for(int i = 0 ; i <n  ; i++){
+            for(int j = 0 ; j <m ; j++){
+                if(board[i][j]==word.charAt(0))q.offer(new int[]{i,j});
+            }
+        }
+        while(!q.isEmpty()){
+            if(dfs(board, word, q.peek()[0],q.peek()[1] , 0, n,m, new boolean[n][m])) return true;
+            q.poll();
+        }
+        return false;
+
+    }
+
+     private boolean dfs(char[][] board, String word,int i , int j, int index , int n , int m, boolean[][] visited){
+           
+
+
+            // if(visited.contains(new int[]{i,j}))return false;
+            
+            if(i<0  || i>=n || j<0 || j>=m || index==word.length() || visited[i][j] ) return false;
+            //  System.out.println(index+"  char at "+word.charAt(index) +"  curr char "+board[i][j]+"   i j "+i+" "+j);
+            
+            if(index==word.length()-1){ 
+                if(board[i][j]==word.charAt(index)){  return true;}
+                else {return false;}
+            }
+            if(board[i][j]==word.charAt(index)){
+             boolean[][] newvisited =visited;
+            for(int x =0 ; x<n; x++) newvisited[x]=Arrays.copyOf(visited[x], m);
+                newvisited[i][j]=true;
+                
+            if(dfs(board, word,i , j+1 , index+1, n,m , newvisited) ||
+            dfs(board, word, i+1 , j , index+1, n,m, newvisited) ||
+            dfs(board, word, i , j-1 , index+1, n,m, newvisited) ||
+            dfs(board, word, i-1 , j , index+1, n,m, newvisited)
+             )return true;
+             else newvisited[i][j]=false;
+            }
+
+            return false;
+            
+
+    }
+}
