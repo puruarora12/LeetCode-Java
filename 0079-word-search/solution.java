e84414e23551d4d5a00ff9f12bf1f2383176962e
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-29 01:00:18 +0000

    LeetCode Sync Runtime - 153 ms (45.93%), Memory - 41.9 MB (32.51%)

--- 0079-word-search/solution.java
+++ 0079-word-search/solution.java
@@ -1,45 +1,43 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        // Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         int n =board.length;
         int m  = board[0].length;
     
         for(int i = 0 ; i <n  ; i++){
             for(int j = 0 ; j <m ; j++){
-                if(board[i][j]==word.charAt(0) && dfs(board, word, i,j , 0))return true;
+                if(board[i][j]==word.charAt(0))q.offer(new int[]{i,j});
             }
         }
+        while(!q.isEmpty()){
+            if(dfs(board, word, q.peek()[0],q.peek()[1] , 0, n,m, new boolean[n][m])) return true;
+            q.poll();
+        }
         return false;
-        // while(!q.isEmpty()){
-        //     if() return true;
-        //     q.poll();
-        // }
-        // return false;
 
     }
 
-     private boolean dfs(char[][] board, String word,int i , int j, int index ){
-           
-            if(index==word.length())return true;
+     private boolean dfs(char[][] board, String word,int i , int j, int index , int n , int m, boolean[][] newvisited){
+           if(index==word.length())return true;
+
 
             // if(visited.contains(new int[]{i,j}))return false;
             
-            if(i<0  || i>=board.length || j<0 || j>=board[0].length || (board[i][j]!=word.charAt(index) ) ) return false;
+            if(i<0  || i>=n || j<0 || j>=m || index==word.length() || newvisited[i][j] || ( board[i][j]!=word.charAt(index) ) ) return false;
             //  System.out.println(index+"  char at "+word.charAt(index) +"  curr char "+board[i][j]+"   i j "+i+" "+j);
             
-
-            // if(board[i][j]==word.charAt(index)){
+            
+            
             // for(int x =0 ; x<n; x++) newvisited[x]=Arrays.copyOf(visited[x], m);
-               char temp = board[i][j];
-               board[i][j]='#';
+                newvisited[i][j]=true;
                 
-            if(dfs(board, word,i , j+1 , index+1 ) ||
-            dfs(board, word, i+1 , j , index+1) ||
-            dfs(board, word, i , j-1 , index+1) ||
-            dfs(board, word, i-1 , j , index+1)
+            if(dfs(board, word,i , j+1 , index+1, n,m , newvisited) ||
+            dfs(board, word, i+1 , j , index+1, n,m, newvisited) ||
+            dfs(board, word, i , j-1 , index+1, n,m, newvisited) ||
+            dfs(board, word, i-1 , j , index+1, n,m, newvisited)
              )return true;
-            board[i][j]=temp;
-            // }
+            newvisited[i][j]=false;
+            
 
             return false;
             
