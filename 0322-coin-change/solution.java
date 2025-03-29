27676f98f6542807090f0960bdd7096aab7e9e2e
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-29 03:31:31 +0000

    LeetCode Sync Runtime - 10 ms (99.43%), Memory - 44.4 MB (71.04%)

--- 0322-coin-change/solution.java
+++ 0322-coin-change/solution.java
@@ -5,14 +5,13 @@ class Solution {
         
         Arrays.fill(am, amount+1);
         am[0]=0;
-        for(int i =1; i<=amount; i++){
-            for(int c: coins){
-                if(i-c>=0)
+        for(int c: coins){
+        for(int i =c; i<=amount; i++){
+                // if(i-c>=0)
                     am[i]=Math.min(am[i], am[i-c]+1);
-                    // System.out.println("for "+i+" value "+am[i]);
+                    
             }
         }
-        // System.out.println(am[amount]);
         if(am[amount]>amount) return -1;
         else return am[amount];
     }
