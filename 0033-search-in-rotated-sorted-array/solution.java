63478de28bc05cb25a15d17d792b54dd4aa0be94
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-29 22:48:56 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 42.1 MB (51.85%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -3,9 +3,7 @@ class Solution {
         int start =0 , end = nums.length-1;
 
         while(start<=end){
-            // System.out.println(end+"   "+start);
             int mid= start+ (end-start)/2;
-            // System.out.println("mid "+mid);
             if(nums[mid]==target) return mid;
 
             if(nums[start]<=nums[mid]){
@@ -16,7 +14,6 @@ class Solution {
                 if(nums[mid]<=target && target<=nums[end])start=mid+1;
                 else end=mid-1;
             }
-            // System.out.println("afte done "+end+"   "+start);
         }
         return -1;
     }
