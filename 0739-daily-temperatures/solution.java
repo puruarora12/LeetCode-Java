dd818c265a5410e10c78407b094d8bf33953f91a
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-29 20:20:55 +0000

    LeetCode Sync Runtime - 61 ms (78.87%), Memory - 63 MB (26.58%)

--- 0739-daily-temperatures/solution.java
+++ 0739-daily-temperatures/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] dailyTemperatures(int[] temps) {
-        Stack<int[]> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         int ans[] = new int[temps.length];
         for(int i =0 ; i<temps.length ; i++){
-            while(!stack.isEmpty() && stack.peek()[0]<temps[i]){
-                int[] cur = stack.pop();
-                ans[cur[1]]=i-cur[1];
+            while(!stack.isEmpty() && temps[stack.peek()]<temps[i]){
+                int cur = stack.pop();
+                ans[cur]=i-cur;
 
             }
-            stack.push(new int[]{temps[i], i});
+            stack.push( i);
         }
         return ans;
     }
