e47b77564634fae5051f47a8964130f88f6f462e
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-29 00:58:38 +0000

    LeetCode Sync Runtime - 128 ms (90.54%), Memory - 41.6 MB (78.31%)

--- 0079-word-search/solution.java
+++ 0079-word-search/solution.java
@@ -6,7 +6,7 @@ class Solution {
     
         for(int i = 0 ; i <n  ; i++){
             for(int j = 0 ; j <m ; j++){
-                if(board[i][j]==word.charAt(0) && dfs(board, word, i,j , 0, n,m))return true;
+                if(board[i][j]==word.charAt(0) && dfs(board, word, i,j , 0))return true;
             }
         }
         return false;
@@ -18,28 +18,28 @@ class Solution {
 
     }
 
-     private boolean dfs(char[][] board, String word,int i , int j, int index , int n , int m){
+     private boolean dfs(char[][] board, String word,int i , int j, int index ){
            
-
+            if(index==word.length())return true;
 
             // if(visited.contains(new int[]{i,j}))return false;
             
-            if(i<0  || i>=n || j<0 || j>=m || index==word.length() || (index==word.length()-1 && board[i][j]!=word.charAt(index) ) ) return false;
+            if(i<0  || i>=board.length || j<0 || j>=board[0].length || (board[i][j]!=word.charAt(index) ) ) return false;
             //  System.out.println(index+"  char at "+word.charAt(index) +"  curr char "+board[i][j]+"   i j "+i+" "+j);
             
-            if(index==word.length()-1) return true;
-            if(board[i][j]==word.charAt(index)){
+
+            // if(board[i][j]==word.charAt(index)){
             // for(int x =0 ; x<n; x++) newvisited[x]=Arrays.copyOf(visited[x], m);
                char temp = board[i][j];
                board[i][j]='#';
                 
-            if(dfs(board, word,i , j+1 , index+1, n,m ) ||
-            dfs(board, word, i+1 , j , index+1, n,m) ||
-            dfs(board, word, i , j-1 , index+1, n,m) ||
-            dfs(board, word, i-1 , j , index+1, n,m)
+            if(dfs(board, word,i , j+1 , index+1 ) ||
+            dfs(board, word, i+1 , j , index+1) ||
+            dfs(board, word, i , j-1 , index+1) ||
+            dfs(board, word, i-1 , j , index+1)
              )return true;
             board[i][j]=temp;
-            }
+            // }
 
             return false;
             
