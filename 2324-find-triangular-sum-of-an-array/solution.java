77d0812a14611d6ea2495695940fb3d762c5b08e
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-30 22:03:09 +0000

    LeetCode Sync Runtime - 41 ms (93.07%), Memory - 45 MB (30.47%)

--- 2324-find-triangular-sum-of-an-array/solution.java
+++ 2324-find-triangular-sum-of-an-array/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int triangularSum(int[] nums) {
-        return checker(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+
+        return checker(nums, nums.length);
     }
-    public int checker(List<Integer> nums){
+    public int checker(int[] nums , int limit){
         // System.out.println(nums);
-        int n = nums.size();
-        if(n==1) return nums.get(0);
-        for(int i = 0 ; i<n-1 ; i++){
-            nums.set(i , (nums.get(i)+nums.get(i+1))%10);
+        // int n = nums.size();
+        if(limit==1) return nums[0];
+        for(int i = 0 ; i<limit-1 ; i++){
+            nums[i]= (nums[i]+nums[i+1])%10;
         }
-        nums.remove(n-1);
-        return checker(nums);
+        return checker(nums, limit-1);
     }
 }
