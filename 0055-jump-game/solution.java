310683ba63cbf3d91cb54e3ee3e1b49f2fd37289
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-03-30 00:52:39 +0000

    LeetCode Sync Runtime - 3 ms (42.67%), Memory - 46.1 MB (21.35%)

--- 0055-jump-game/solution.java
+++ 0055-jump-game/solution.java
@@ -5,12 +5,15 @@ class Solution {
         dp[n-1]=true;
         int last = n-1;
         for(int i =n-2 ; i>=0; i--){
-            if(i+nums[i]>=nums.length || i+nums[i]>=last ){ dp[i]=true; last = i;}
-            else if(i+nums[i]<last)dp[i]=false; 
-            else { 
-                dp[i]=dp[nums[i]+i];
-                last =dp[i] ? i :last;
+            if(i+nums[i]>=nums.length || i+nums[i]>=last ){ 
+                dp[i]=true; 
+                last = i;
             }
+            else if(i+nums[i]<last)dp[i]=false; 
+            // else { 
+            //     dp[i]=dp[nums[i]+i];
+            //     last =dp[i] ? i :last;
+            // }
         }
         return dp[0];
     }
