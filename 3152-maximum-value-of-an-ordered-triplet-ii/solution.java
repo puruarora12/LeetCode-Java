8d916c8bc3aa8014ed3bceee29e4967ec7989384
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-04-03 21:23:08 +0000

    LeetCode Sync Runtime - 4 ms (7.48%), Memory - 62.7 MB (15.56%)

--- 3152-maximum-value-of-an-ordered-triplet-ii/solution.java
+++ 3152-maximum-value-of-an-ordered-triplet-ii/solution.java
@@ -1,28 +1,27 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-      int max = nums[0];
-        long maxDiff = 0 ;
-        long maxTriplet = 0 ;
-        //int maxs[] = new int[nums.length];
-        int mins[] = new int[nums.length];
-        //maxs[0]=nums[0];
-        mins[nums.length-1] = nums[nums.length-1];
-        
+        long ans = 0;
+        int max = nums[0];
+        int[] maxs = new int[nums.length];
+        int[] mins = new int[nums.length];
+        Arrays.fill(maxs,0);
+        Arrays.fill(mins,0);
+        maxs[nums.length-1]=nums[nums.length-1];
+
         for(int i = nums.length-2 ; i>=0 ; i--){
-           mins[i] = Math.max(mins[i+1], nums[i] ); 
+            //  System.out.println("Max at "+i+"  : "+nums[maxs[i]]+"  min :"+nums[mins[i]]);
+            //  System.out.println("Max at -1 values "+(i-1)+"  : "+nums[maxs[i-1]]+"  min :"+nums[mins[i-1]]);
+            maxs[i]  = Math.max(maxs[i+1], nums[i]);
+
+            // System.out.println("Max at "+i+"  : "+maxs[i]);//+"  min :"+nums[mins[i]]);
         }
-        
-        
-        for(int i =1 ; i <nums.length-1 ; i++){
-            //System.out.println("max "+max + "   nums[i] "+nums[i]+"  mins[i+1] "+mins[i+1]);
-            long diff = (max - nums[i]);
-                diff*=mins[i+1];       
-            maxDiff = Math.max(diff, maxDiff);
-            
-            
-            max = Math.max(nums[i], max);
-        }   
-        
-        return maxDiff;
+        long large = nums[0];
+        for(int i =0 ; i<nums.length-1 ;i++){
+            // System.out.println("the calc is "+(large - nums[i])* maxs[nums.length-1] );
+            ans = Math.max(ans, (large - nums[i])* maxs[i+1] );
+            large = Math.max(nums[i], large);
+
+        }
+        return ans;
     }
 }
