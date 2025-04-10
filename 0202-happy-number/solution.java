74b7e4a8ec50647536b408b327743ed02d73fa7a
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-04-10 21:56:13 +0000

    LeetCode Sync Runtime - 3 ms (5.65%), Memory - 41 MB (29.15%)

--- 0202-happy-number/solution.java
+++ 0202-happy-number/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> s = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         while(n!=1){
-            int temp = 0 ; 
-            while(n>0){
-                temp += (n%10)*(n%10);
-                n/=10;
-            }
-            n=temp;
-            if(s.contains(n)) return false;
-            s.add(n);
+            if(set.contains(n))return false;
+            long temp = 0;
+            set.add(n);
+            for(char c: (n+"").toCharArray())temp+=Math.pow((c-'0'), 2);
+            n=(int)temp;
+            // if(n==og) return false;
         }
-        return true;
+
+
+    return n==1? true: false;    
     }
 }
