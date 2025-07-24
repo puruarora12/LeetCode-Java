e91345d535349ea197e95e0ff7d53cd4bb294d63
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-07-24 17:51:32 +0000

    LeetCode Sync Runtime - 3 ms (50.70%), Memory - 19 MB (23.32%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
         for index,value in enumerate(nums):
-            # print(f" index {index} with value {value} and remaining list is {nums[index+1:]} and target-value is {target-value}")
-            nextHalf =nums[index+1:]
-            if target-value in nextHalf:
-                return [index, 1+index+nextHalf.index(target-value)] 
-
-        return [0,0]
+            diff = target-value
+            if diff in map:
+                return [index, map[diff] ]
+            
+            map[value]=index
