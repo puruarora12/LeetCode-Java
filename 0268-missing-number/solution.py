eaeda54e74f9e239f51ecdb9ce9da89b4d9d6098
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-07-24 16:44:36 +0000

    LeetCode Sync Runtime - 1282 ms (14.83%), Memory - 18.7 MB (71.83%)

---
+++ 0268-missing-number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(0, len(nums)+1):
+            if i not in nums:
+                return i
+        return len(nums)
+
+        
