320d865193da5958b467db8859e4c68fd7373958
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-08-24 23:58:02 +0000

    LeetCode Sync Runtime - 2 ms (82.98%), Memory - 45.6 MB (35.34%)

--- 0130-surrounded-regions/solution.java
+++ 0130-surrounded-regions/solution.java
@@ -5,33 +5,34 @@ class Solution {
         // System.out.println(m+"   "+n);
         List<String> filled = new ArrayList<>();
         for(int i = 0 ; i<m; i++){
-            if(board[i][0]=='O') helper(board, i, 0 , filled);
-            if(board[i][n-1]=='O') helper(board, i , n-1 , filled);
+            if(board[i][0]=='O') helper(board, i, 0 );
+            if(board[i][n-1]=='O') helper(board, i , n-1 );
         }
         for(int j = 0 ; j<n; j++){
-            if(board[0][j]=='O') helper(board, 0, j , filled);
-            if(board[m-1][j]=='O') helper(board,m-1 , j , filled);
+            if(board[0][j]=='O') helper(board, 0, j );
+            if(board[m-1][j]=='O') helper(board,m-1 , j );
         }
 
         for(int i = 0 ; i <m ; i++){
             for(int j = 0 ; j<n ; j++){
-                if(board[i][j]=='O' && !filled.contains(i+","+j)) board[i][j]='X';
+                if(board[i][j]=='O' ) board[i][j]='X';
+                if(board[i][j]=='T') board[i][j]='O';
             }
         }
     }
 
-    public static void helper(char[][] board, int i , int j , List<String> filled){
+    public static void helper(char[][] board, int i , int j ){
         // System.out.println("starting "+i+ "    "+j);
-        if(i<0 || i>board.length-1 || j<0 || j>board[0].length-1 || filled.contains(i+","+j)) return;
+        if(i<0 || i>board.length-1 || j<0 || j>board[0].length-1 ) return;
         if(board[i][j]=='O'){
 
-        
-        filled.add(i+","+j);
+        board[i][j]='T';
+        // filled.add(i+","+j);
         // System.out.println("filled for "+i+"   "+j);
-        helper(board, i+1, j , filled);
-        helper(board, i-1, j , filled);
-        helper(board , i , j+1 , filled);
-        helper(board, i , j-1 , filled);
+        helper(board, i+1, j );
+        helper(board, i-1, j );
+        helper(board , i , j+1 );
+        helper(board, i , j-1 );
         }
         return;
     }
