3c574aef5b72f1e2a57713b645e8d4676b3528cc
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-08-24 23:54:14 +0000

    LeetCode Sync Runtime - 232 ms (5.17%), Memory - 45.6 MB (46.01%)

--- 0130-surrounded-regions/solution.java
+++ 0130-surrounded-regions/solution.java
@@ -1,34 +1,38 @@
 class Solution {
-    public static boolean edge= false;
     public void solve(char[][] board) {
-        for (int i= 0 ; i<board.length ; i++){
-            for(int j= 0 ; j<board[0].length ; j++){
-                if(board[i][j]=='O'){
-                    Set<String> region = new HashSet<>();
-                    edge= true;
-                    helper(board, i , j, region);
-                    if( edge) for (String x: region) board[Integer.parseInt(x.split(",")[0])][Integer.parseInt(x.split(",")[1])]='X';
-                    // System.out.println(region);
-                }
+        int m = board.length; 
+        int n = board[0].length;
+        // System.out.println(m+"   "+n);
+        List<String> filled = new ArrayList<>();
+        for(int i = 0 ; i<m; i++){
+            if(board[i][0]=='O') helper(board, i, 0 , filled);
+            if(board[i][n-1]=='O') helper(board, i , n-1 , filled);
+        }
+        for(int j = 0 ; j<n; j++){
+            if(board[0][j]=='O') helper(board, 0, j , filled);
+            if(board[m-1][j]=='O') helper(board,m-1 , j , filled);
+        }
+
+        for(int i = 0 ; i <m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(board[i][j]=='O' && !filled.contains(i+","+j)) board[i][j]='X';
             }
         }
     }
 
-   public static int[][] dirs = new int[][]{{1,0} ,{-1,0}, {0,1}, {0,-1}}; 
-   public static void helper(char board[][],  int i , int j , Set<String> region){
-        // System.out.println(i+"       "+j);
-        if(i==0 || i==board.length-1 || j==0 || j==board[i].length-1) {edge= false;return ;}
-        region.add(i+","+j);
-        // System.out.println("region added");
-        // region.add(new int[]{100,100});
-        // System.out.println(region.contains(new int[]{100,100}));
-        for( int[] dir : dirs){
-            if(board[i+dir[0]][j+dir[1]]=='O' && !region.contains((i+dir[0])+","+(j+dir[1])) ) helper(board, i+dir[0], j+dir[1], region);
+    public static void helper(char[][] board, int i , int j , List<String> filled){
+        // System.out.println("starting "+i+ "    "+j);
+        if(i<0 || i>board.length-1 || j<0 || j>board[0].length-1 || filled.contains(i+","+j)) return;
+        if(board[i][j]=='O'){
 
-    
-        }
-        // System.out.println("returning true");
-        return ;
         
-   } 
+        filled.add(i+","+j);
+        // System.out.println("filled for "+i+"   "+j);
+        helper(board, i+1, j , filled);
+        helper(board, i-1, j , filled);
+        helper(board , i , j+1 , filled);
+        helper(board, i , j-1 , filled);
+        }
+        return;
+    }
 }
