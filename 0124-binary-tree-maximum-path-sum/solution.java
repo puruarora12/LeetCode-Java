a832e1636bb5135b8181cf5345461bc64705f652
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-08-30 00:51:24 +0000

    LeetCode Sync Runtime - 0 ms (100.00%), Memory - 44.5 MB (36.84%)

--- 0124-binary-tree-maximum-path-sum/solution.java
+++ 0124-binary-tree-maximum-path-sum/solution.java
@@ -1,22 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int maxSum = Integer.MIN_VALUE;
-
+    public int maxSum = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         helper(root);
         return maxSum;
     }
+    public int helper(TreeNode root ){
+        if(root==null)return 0;
+        int left = Math.max(helper(root.left), 0);
+        int right = Math.max(helper(root.right), 0);
 
-    private int helper(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int leftMaxPath = Math.max(helper(node.left), 0);
-        int rightMaxPath = Math.max(helper(node.right), 0);
-
-        int maxIfNodeIsRoot = node.val + leftMaxPath + rightMaxPath;
-        maxSum = Math.max(maxSum, maxIfNodeIsRoot);
-
-        return node.val + Math.max(leftMaxPath, rightMaxPath);
+        int nodeRoot = root.val+left+right;
+        maxSum = Math.max(maxSum, nodeRoot);
+        return root.val+ Math.max(left,right);
     }
 }
