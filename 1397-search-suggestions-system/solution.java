2da9837fcb8d8c9cc29da0c229fa8e7aa49026e3
Puru Arora <36695319+puruarora12@users.noreply.github.com>
2025-09-01 03:34:45 +0000

    LeetCode Sync Runtime - 27 ms (48.15%), Memory - 48.1 MB (48.63%)

--- 1397-search-suggestions-system/solution.java
+++ 1397-search-suggestions-system/solution.java
@@ -11,16 +11,33 @@ class Solution {
             tosearch+=i;
             List<String> preMatches = map.get(word);
             List<String> matches = new ArrayList<>();
+            int index =0 ;
             for(String match : preMatches){
-                if (match.startsWith(tosearch))
+                if (match.startsWith(tosearch)){
                     matches.add(match);
-                
+                    index++;
+                }
+                if(index==3){
+                    // System.out.println("added in main  here");
+                     List<String> toadd = new ArrayList<>(matches);
+                     ans.add(toadd);
+                     index++;
+                }
             }
-            List<String> toadd = new ArrayList<>();
-            for(int index =0 ; index<matches.size() && index<3 ; index++ )toadd.add(matches.get(index));
-            ans.add(toadd);
-            word+=i;
+             word+=i;
             map.put(word ,matches);
+            // System.out.println(index);
+            // System.out.println(word +  " matches : "+matches);
+            if(index<3){
+                // System.out.println("adding here in outside ");
+                  List<String> toadd = new ArrayList<>(matches);
+                     ans.add(toadd);
+            }
+            // List<String> toadd = new ArrayList<>();
+            // for(int index =0 ; index<matches.size() && index<3 ; index++ )toadd.add(matches.get(index));
+            // ans.add(toadd);
+            // word+=i;
+            // map.put(word ,matches);
 
            
         }
